b74085274560d9e7c4976779fc2031246a5951da
MartinWheelerMT <martin.wheeler@madetech.com>
2022-07-11 15:48:52 +0100

    PCAT#100521 - API Create Advisory Board Decision - initial domain classes

---
+++ Dfe.Academies.Academisation.Domain/AdvisoryBoardDecisionAggregate/AdvisoryBoardDecision.cs
@@ -0,0 +1,30 @@
+﻿using Dfe.Academies.Academisation.IDomain.AdvisoryBoardDecisionAggregate;
+using FluentValidation;
+
+namespace Dfe.Academies.Academisation.Domain.AdvisoryBoardDecisionAggregate;
+
+public class AdvisoryBoardDecision : IAdvisoryBoardDecision
+{
+    private static readonly CreateAdvisoryBoardDecisionValidator CreateValidator = new();
+    
+    public int Id { get; set; }
+    public IAdvisoryBoardDecisionDetails Details { get; }
+    
+    private AdvisoryBoardDecision(IAdvisoryBoardDecisionDetails details)
+    {
+        Details = details;
+    }
+
+    internal static async Task<AdvisoryBoardDecision> Create(IAdvisoryBoardDecisionDetails details)
+    {
+        var validationResult = await CreateValidator.ValidateAsync(details);
+
+        if (validationResult.IsValid)
+        {
+            return new(details);
+        }
+
+        throw new ValidationException(validationResult.ToString());
+    }
+    
+}
---
+++ Dfe.Academies.Academisation.IDomain/AdvisoryBoardDecisionAggregate/AdvisoryBoardDeclinedReasons.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfe.Academies.Academisation.IDomain.AdvisoryBoardDecisionAggregate;
+
+public enum AdvisoryBoardDeclinedReasons
+{
+    Finance = 0,
+    Performance = 1,
+    Other = 2
+}
