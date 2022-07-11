e501f48c59bfde6ded0467328fb388b51a22fa11
MartinWheelerMT <martin.wheeler@madetech.com>
2022-07-11 15:56:14 +0100

    PCAT#100521 - API Create Advisory Board Decision - add factory

---
+++ Dfe.Academies.Academisation.Domain/AdvisoryBoardDecisionAggregate/AdvisoryBoardDecisionFactory.cs
@@ -0,0 +1,11 @@
+﻿using Dfe.Academies.Academisation.IDomain.AdvisoryBoardDecisionAggregate;
+
+namespace Dfe.Academies.Academisation.Domain.AdvisoryBoardDecisionAggregate;
+
+public class AdvisoryBoardDecisionFactory : IAdvisoryBoardDecisionFactory
+{
+    public async Task<IAdvisoryBoardDecision> Create(IAdvisoryBoardDecisionDetails details)
+    {
+        return await AdvisoryBoardDecision.Create(details);
+    }
+}
