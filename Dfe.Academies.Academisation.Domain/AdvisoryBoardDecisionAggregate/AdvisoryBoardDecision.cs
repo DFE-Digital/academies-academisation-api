c7bb9008dc26b1b84dddb2c09edca0630c1b2039
MartinWheelerMT <martin.wheeler@madetech.com>
2022-07-11 19:04:34 +0100

    PCAT#100521 - API Create Advisory Board Decision - Add Project Id to AdvisoryBoardDecision Update tests

--- Dfe.Academies.Academisation.Domain/AdvisoryBoardDecisionAggregate/AdvisoryBoardDecision.cs
+++ Dfe.Academies.Academisation.Domain/AdvisoryBoardDecisionAggregate/AdvisoryBoardDecision.cs
@@ -7,21 +7,23 @@ public class AdvisoryBoardDecision : IAdvisoryBoardDecision
 {
     private static readonly CreateAdvisoryBoardDecisionValidator CreateValidator = new();
     
+    public int ProjectId { get; }
     public int Id { get; set; }
     public IAdvisoryBoardDecisionDetails Details { get; }
     
-    private AdvisoryBoardDecision(IAdvisoryBoardDecisionDetails details)
+    private AdvisoryBoardDecision(int projectId, IAdvisoryBoardDecisionDetails details)
     {
+        ProjectId = projectId;
         Details = details;
     }
 
-    internal static async Task<AdvisoryBoardDecision> Create(IAdvisoryBoardDecisionDetails details)
+    internal static async Task<AdvisoryBoardDecision> Create(int projectId, IAdvisoryBoardDecisionDetails details)
     {
         var validationResult = await CreateValidator.ValidateAsync(details);
 
         if (validationResult.IsValid)
         {
-            return new(details);
+            return new(projectId, details);
         }
 
         throw new ValidationException(validationResult.ToString());
