9fd51beaf984aa7ca6aefd3739e797d6baf17221
MartinWheelerMT <martin.wheeler@madetech.com>
2022-07-14 12:37:07 +0100

    AB#100251 - Record a ConversionDecision - Add Conversion Project, Factories and Methods

---
+++ Dfe.Academies.Academisation.Domain/ConversionProjectAggregate/ConversionProjectFactory.cs
@@ -0,0 +1,13 @@
+﻿using Dfe.Academies.Academisation.IDomain.ConversionProjectAggregate;
+
+namespace Dfe.Academies.Academisation.Domain.ConversionProjectAggregate;
+
+public class ConversionProjectFactory : IConversionProjectFactory
+{
+    public ConversionProjectFactory() { }
+    
+    public async Task<IConversionProject> Create(int projectId)
+    {
+        return await ConversionProject.Create(projectId);
+    }
+}
---
+++ Dfe.Academies.Academisation.IDomain/ConversionProjectAggregate/IConversionProjectFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace Dfe.Academies.Academisation.IDomain.ConversionProjectAggregate;
+
+public interface IConversionProjectFactory
+{
+    Task<IConversionProject> Create(int projectId);
+}
