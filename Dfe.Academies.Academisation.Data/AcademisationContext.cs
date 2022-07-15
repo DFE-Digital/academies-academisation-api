997ebbc6a2d1b511bb464f515f3746ed00328ffa
MartinWheelerMT <martin.wheeler@madetech.com>
2022-07-15 15:29:09 +0100

    AB#90919 - API Create Application - Add Entity Framework, AcademisationContext and State models

---
+++ Dfe.Academies.Academisation.Data/AcademisationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dfe.Academies.Academisation.Data;
+
+public class AcademisationContext : DbContext
+{
+	public DbSet<ConversionApplicationState> ConversionApplications { get; set; }
+	public DbSet<ContributorState> Contributors { get; set; }
+	
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+	}
+}
---
+++ Dfe.Academies.Academisation.Data/ConversionApplicationState.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+using Dfe.Academies.Academisation.Domain.Core;
+using Dfe.Academies.Academisation.IDomain.ConversionApplicationAggregate;
+
+namespace Dfe.Academies.Academisation.Data;
+
+[Table(name: "application", Schema = "acad")]
+public class ConversionApplicationState
+{
+	public ConversionApplicationState(IConversionApplication conversionApplication)
+	{
+		ApplicationType = conversionApplication.ApplicationType;
+		Contributors = conversionApplication.Contributors
+			.Select(contributor => new ContributorState(contributor.Details))
+			.ToHashSet();
+	}
+
+	public int ConversionApplicationId { get; set; }
+	public ApplicationType ApplicationType { get; set; }
+	
+	public HashSet<ContributorState> Contributors { get; set; }
+}
+
