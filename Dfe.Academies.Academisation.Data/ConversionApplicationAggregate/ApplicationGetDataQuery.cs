d3e9b9d06bdae4762c7ca0fe69aae10a1f290ee8
Catherine LAWLOR <Catherine.LAWLOR@education.gov.uk>
2022-07-19 16:04:16 +0100

    AB#92463 Get Application - entity framework get

--- Dfe.Academies.Academisation.Data/ConversionApplicationAggregate/ApplicationGetDataQuery.cs
+++ Dfe.Academies.Academisation.Data/ConversionApplicationAggregate/ApplicationGetDataQuery.cs
@@ -1,16 +1,22 @@
-﻿using Dfe.Academies.Academisation.Domain.ConversionApplicationAggregate;
-using Dfe.Academies.Academisation.Domain.Core;
-using Dfe.Academies.Academisation.IData.ConversionApplicationAggregate;
+﻿using Dfe.Academies.Academisation.IData.ConversionApplicationAggregate;
 using Dfe.Academies.Academisation.IDomain.ConversionApplicationAggregate;
 
 namespace Dfe.Academies.Academisation.Data.ConversionApplicationAggregate
 {
 	public class ApplicationGetDataQuery : IApplicationGetDataQuery
 	{
+		private readonly AcademisationContext _context;
+
+		public ApplicationGetDataQuery(AcademisationContext context)
+		{
+			_context = context;
+		}
+
 		public async Task<IConversionApplication> Execute(int id)
 		{
-			await Task.CompletedTask;
-			return new ConversionApplication(1, ApplicationType.JoinAMat, new Dictionary<int, ContributorDetails>());
+			ConversionApplicationState conversionApplicationState =  await _context.ConversionApplications.FindAsync(id) ?? throw new ArgumentException($"Entity with value {id} not found");
+
+			return conversionApplicationState.MapToDomain();
 		}
 	}
 }
