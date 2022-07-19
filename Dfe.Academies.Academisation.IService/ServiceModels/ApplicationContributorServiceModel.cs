caa9b8d7d7da8df05553b9e736370d53aef4ebf8
MartinWheelerMT <martin.wheeler@madetech.com>
2022-07-19 12:39:45 +0100

    AB#90919 - API Application Create - create and update service models and mappers

---
+++ Dfe.Academies.Academisation.IService/ServiceModels/ApplicationContributorServiceModel.cs
@@ -0,0 +1,13 @@
+﻿using Dfe.Academies.Academisation.Domain.Core;
+
+namespace Dfe.Academies.Academisation.IService.ServiceModels;
+
+public class ApplicationContributorServiceModel
+{
+	public int ContributorId { get; set; }
+	public string FirstName { get; set; }
+	public string LastName  { get; set; }
+	public string EmailAddress  { get; set; }
+	public ContributorRole Role  { get; set; }
+	public string? OtherRoleName  { get; set; }
+}
---
+++ Dfe.Academies.Academisation.Service/Mappers/ApplicationServiceModelMapper.cs
@@ -0,0 +1,19 @@
+﻿using Dfe.Academies.Academisation.IDomain.ConversionApplicationAggregate;
+using Dfe.Academies.Academisation.IService.ServiceModels;
+
+namespace Dfe.Academies.Academisation.Service.Mappers;
+
+internal static class ApplicationServiceModelMapper
+{
+	internal static ApplicationServiceModel FromDomain(IConversionApplication conversionApplication)
+	{
+		return new()
+		{
+			ApplicationId = conversionApplication.ApplicationId,
+			ApplicationType = conversionApplication.ApplicationType,
+			Contributors = conversionApplication.Contributors
+				.Select(ApplicationContributorServiceModelMapper.FromDomain)
+				.ToList()
+		};
+	}
+}
