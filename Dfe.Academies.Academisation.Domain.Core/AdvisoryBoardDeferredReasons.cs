245fa8b78f7638b35b7ff6094d202a1107273280
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-07-21 17:18:07 +0100

    AB#100708 - AdvisoryBoardDecision - Add new controller for recording advisory board decisions

---
+++ Dfe.Academies.Academisation.Domain.Core/AdvisoryBoardDeferredReasons.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.Academies.Academisation.Domain.Core;
+
+public enum AdvisoryBoardDeferredReasons
+{
+    AdditionalInformationNeeded = 0,
+    LocalSensitivityConcerns = 1,
+    PerformanceConcerns = 2,
+    TrustToEngageMoreWithStakeholders = 3,
+    Other = 4
+}
--- Dfe.Academies.Academisation.IDomain/ConversionProjectAggregate/AdvisoryBoardDeclinedReasons.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dfe.Academies.Academisation.IDomain.ConversionProjectAggregate;
-
-public enum AdvisoryBoardDeclinedReasons
-{
-	Finance = 0,
-	Performance = 1,
-	Other = 2
-}
