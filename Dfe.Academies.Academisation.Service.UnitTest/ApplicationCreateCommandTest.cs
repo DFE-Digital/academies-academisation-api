5b94787de7aaeffa3d8742406277940985ef96a2
Catherine LAWLOR <Catherine.LAWLOR@education.gov.uk>
2022-07-22 12:39:55 +0100

    AB#90919 Create Application - service layer happy path test

--- Dfe.Academies.Academisation.Service.UnitTest/ApplicationCreateCommandTest.cs
+++ Dfe.Academies.Academisation.Service.UnitTest/ApplicationCreateCommandTest.cs
@@ -1,5 +1,8 @@
-﻿using Dfe.Academies.Academisation.Domain.Core;
+﻿using Bogus;
+using Dfe.Academies.Academisation.Domain.Core;
+using Dfe.Academies.Academisation.IData.ConversionApplicationAggregate;
 using Dfe.Academies.Academisation.IDomain.ConversionApplicationAggregate;
+using Dfe.Academies.Academisation.IService.ServiceModels;
 using Moq;
 using Xunit;
 
@@ -7,25 +10,38 @@ namespace Dfe.Academies.Academisation.Service.UnitTest
 {
 	public class ApplicationCreateCommandTest
 	{
+		private readonly Faker _faker = new Faker();
 		private static Mock<IConversionApplicationFactory> _conversionApplicationFactoryMock = new ();
-		private static Mock<ContributorDetails> _contributerDetails = new ();
+		private static Mock<IApplicationCreateDataCommand> _applicationCreateDataCommandMock = new ();
 
-		//[Theory]
-		//[InlineData(ApplicationType.FormAMat)]
-		//[InlineData(ApplicationType.JoinAMat)]
-		//public async void Create_CallsFactoryCreate_WithCorrectValues(ApplicationType applicationType)
-		//{
-		//	ApplicationCreateCommand createCommand = new(_conversionApplicationFactoryMock.Object);
-
-		//	await createCommand.Create(applicationType, _contributerDetails.Object);
-
-		//	_conversionApplicationFactoryMock
-		//		.Verify(x =>	x.Create(It.Is<ApplicationType>(y => y == applicationType), It.Is<IContributorDetails>(y => y == _contributerDetails.Object)), Times.Once());
-		//}
-
-		public async void Create_CallsRepository_WithCorrectValues()
+		[Theory]
+		[InlineData(ApplicationType.FormAMat)]
+		[InlineData(ApplicationType.JoinAMat)]
+		public async void ApplicationReturnedFromFactory___ApplicationPassedToDataLayer(ApplicationType applicationType)
 		{
-			await Task.CompletedTask;
+			// arrange
+			var applicationCreateRequestModel = new ApplicationCreateRequestModelBuilder()
+				.WithApplicationType(applicationType)
+				.Build();
+
+			Mock<IConversionApplication> conversionApplicationMock = new Mock<IConversionApplication>();
+			conversionApplicationMock.SetupGet(x => x.Contributors)
+				.Returns(new List<IContributor>().AsReadOnly());
+
+			_conversionApplicationFactoryMock
+				.Setup(x => x.Create(It.IsAny<ApplicationType>(), It.IsAny<ContributorDetails>()))
+				.ReturnsAsync(conversionApplicationMock.Object);
+
+			ApplicationCreateCommand sut = new(_conversionApplicationFactoryMock.Object, _applicationCreateDataCommandMock.Object);
+
+			// act
+			var result = await sut.Execute(applicationCreateRequestModel);
+
+			// assert
+			_applicationCreateDataCommandMock
+				.Verify(x => x.Execute(It.Is<IConversionApplication>(y => y == conversionApplicationMock.Object)), Times.Once());
+
+			Assert.IsType<ApplicationServiceModel>(result);
 		}
 	}
 }
