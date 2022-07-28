9c865ce34ff35de6ee6b86718cfb0032961209c5
Catherine LAWLOR <Catherine.LAWLOR@education.gov.uk>
2022-07-28 15:59:38 +0100

    AB#90919 API Create Application - Command result classes

---
+++ Dfe.Academies.Academisation.Core/CommandValidationErrorResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfe.Academies.Academisation.Core;
+
+public class CommandValidationErrorResult : CommandResult
+{
+	public CommandValidationErrorResult(IEnumerable<ValidationError> validationErrors)
+	{
+		ValidationErrors = validationErrors.ToList().AsReadOnly();
+	}
+
+	public IReadOnlyCollection<ValidationError> ValidationErrors { get; }
+}
