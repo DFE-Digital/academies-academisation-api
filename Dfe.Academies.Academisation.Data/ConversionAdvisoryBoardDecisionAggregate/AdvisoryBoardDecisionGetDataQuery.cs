3d3460f99b48e5aed8651dc2f70cee861f19f84e
MartinWheelerMT <martin.wheeler@madetech.com>
2022-08-02 14:25:06 +0100

    AB#100708 - ConversionAdvisoryBoardDecision - Add AdvisoryBoardDecisionGetDataQuery Add Tests for Query Update XUnit in Data.Test project

---
+++ Dfe.Academies.Academisation.Data/ConversionAdvisoryBoardDecisionAggregate/AdvisoryBoardDecisionGetDataQuery.cs
@@ -0,0 +1,27 @@
+﻿using Dfe.Academies.Academisation.IData.ConversionAdvisoryBoardDecisionAggregate;
+using Dfe.Academies.Academisation.IDomain.ConversionAdvisoryBoardDecisionAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dfe.Academies.Academisation.Data.ConversionAdvisoryBoardDecisionAggregate
+{
+	public class AdvisoryBoardDecisionGetDataQuery : IAdvisoryBoardDecisionGetDataQuery
+	{
+		private readonly AcademisationContext _context;
+
+		public AdvisoryBoardDecisionGetDataQuery(AcademisationContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IConversionAdvisoryBoardDecision?> Execute(int id)
+		{
+			var state = await _context.ConversionAdvisoryBoardDecisions
+				.Include(s => s.DeclinedReasons)
+				.Include(s => s.DeferredReasons)
+				.AsNoTracking()
+				.SingleOrDefaultAsync(s => s.Id == id);
+			
+			return state?.MapToDomain();
+		}
+	}
+}
