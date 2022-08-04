f04b4107df6b1e591f1f268c3b7d03d43ed9a29b
Wystan Palm <wystan.palm@madetech.com>
2022-08-04 16:52:24 +0100

    AB#92464 API Update Application - database migration for ApplicationSchool

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220804154503_AddApplicationSchool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class AddApplicationSchool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationSchool",
+                schema: "academisation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Urn = table.Column<int>(type: "int", nullable: false),
+                    ConversionApplicationId = table.Column<int>(type: "int", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationSchool", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationSchool_ConversionApplication_ConversionApplicationId",
+                        column: x => x.ConversionApplicationId,
+                        principalSchema: "academisation",
+                        principalTable: "ConversionApplication",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationSchool_ConversionApplicationId",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                column: "ConversionApplicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationSchool",
+                schema: "academisation");
+        }
+    }
+}
