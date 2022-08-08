dd7fc4816a062dc0e1f39c7d6e5c21243efb3c6e
Wystan Palm <wystan.palm@madetech.com>
2022-08-08 13:04:32 +0100

    AB#92464 API Update Application - domain validation, type not changed

--- Dfe.Academies.Academisation.Domain.UnitTest/ApplicationAggregate/ApplicationUpdateTests.cs
+++ Dfe.Academies.Academisation.Domain.UnitTest/ApplicationAggregate/ApplicationUpdateTests.cs
@@ -1,9 +1,10 @@
-﻿using AutoFixture;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
 using Dfe.Academies.Academisation.Core;
 using Dfe.Academies.Academisation.Domain.ApplicationAggregate;
 using Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate;
-using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace Dfe.Academies.Academisation.Domain.UnitTest.ApplicationAggregate;
@@ -30,8 +31,29 @@ public class ApplicationUpdateTests
 		Assert.Single(validationErrorResult.ValidationErrors, v => v.PropertyName == "ApplicationStatus");
 	}
 
+	[Theory]
+	[MemberData(nameof(BuildRandomDifferentTypes))]
+	public void ApplicationTypeChanged___ValidationErrorReturned(ApplicationType updated, ApplicationType existing)
+	{
+		// arrange
+		var subject = BuildApplication(ApplicationStatus.InProgress, existing);
+		var expectedPropertyName = nameof(Application.ApplicationType);
+
+		// act
+		var result = subject.Update(
+			updated,
+			subject.ApplicationStatus,
+			subject.Contributors.ToDictionary(c => c.Id, c => c.Details),
+			subject.Schools.ToDictionary(s => s.Id, s => s.Details));
+
+		// assert
+		var validationErrorResult = Assert.IsAssignableFrom<CommandValidationErrorResult>(result);
+		var error = Assert.Single(validationErrorResult.ValidationErrors);
+		Assert.Equal(expectedPropertyName, error.PropertyName);
+	}
+
 	[Fact]
-	public void ApplicationInPrograss___SuccessReturned()
+	public void ExistingInProgress_TypeUnchanged___SuccessReturned()
 	{
 		// arrange
 		var subject = BuildApplication(ApplicationStatus.InProgress);
@@ -47,16 +69,27 @@ public class ApplicationUpdateTests
 		Assert.IsAssignableFrom<CommandSuccessResult>(result);
 	}
 
-	private Application BuildApplication(ApplicationStatus applicationStatus)
+	private Application BuildApplication(ApplicationStatus applicationStatus, ApplicationType? type = null)
 	{
 		Application application = new(
 			_fixture.Create<int>(),
-			_fixture.Create<ApplicationType>(),
+			type ?? _fixture.Create<ApplicationType>(),
 			applicationStatus,
 			_fixture.Create<Dictionary<int, ContributorDetails>>(),
 			_fixture.Create<Dictionary<int, SchoolDetails>>());
 
 		return application;
+	}
+
+	private static IEnumerable<object[]> BuildRandomDifferentTypes()
+	{
+		var existing = new Fixture().Create<ApplicationType>();
+
+		var types = Enum.GetValues(typeof(ApplicationType)).Cast<ApplicationType>().Where(t => t != existing);
+		Random random = new();
+		var updated = types.ElementAt(random.Next(1, types.Count()));
+
+		yield return new object[] { updated, existing };
 	}
 }
 
--- Dfe.Academies.Academisation.Domain/ApplicationAggregate/UpdateApplicationValidator.cs
+++ Dfe.Academies.Academisation.Domain/ApplicationAggregate/UpdateApplicationValidator.cs
@@ -17,5 +17,11 @@ internal class UpdateApplicationValidator
 			.Equal(ApplicationStatus.InProgress)
 			.WithMessage("Application must be InProgress to use Update")
 			.OverridePropertyName(nameof(Application.ApplicationStatus));
+
+		RuleFor(x => x.existing.ApplicationType)
+			.Must(x => false)
+			.When(x => x.type != x.existing.ApplicationType)
+			.WithMessage("Cannot change the status using this operation. To submit, use the Submit operation.")
+			.OverridePropertyName(nameof(Application.ApplicationType));
 	}
 }
