33847c0153178f4d83bdc635d8a3db92ad7ee46a
Catherine LAWLOR <Catherine.LAWLOR@education.gov.uk>
2022-08-09 15:58:59 +0100

    AB#92464 API Update Application - Data layer - School pupil numbers

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220809145351_AddApplicationSchool_PupilNumbers.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class AddApplicationSchool_PupilNumbers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectedPupilNumbersYear1",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectedPupilNumbersYear2",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectedPupilNumbersYear3",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProposedNewSchoolName",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SchoolCapacityAssumptions",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SchoolCapacityPublishedAdmissionsNumber",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectedPupilNumbersYear1",
+                schema: "academisation",
+                table: "ApplicationSchool");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectedPupilNumbersYear2",
+                schema: "academisation",
+                table: "ApplicationSchool");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectedPupilNumbersYear3",
+                schema: "academisation",
+                table: "ApplicationSchool");
+
+            migrationBuilder.DropColumn(
+                name: "ProposedNewSchoolName",
+                schema: "academisation",
+                table: "ApplicationSchool");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolCapacityAssumptions",
+                schema: "academisation",
+                table: "ApplicationSchool");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolCapacityPublishedAdmissionsNumber",
+                schema: "academisation",
+                table: "ApplicationSchool");
+        }
+    }
+}
