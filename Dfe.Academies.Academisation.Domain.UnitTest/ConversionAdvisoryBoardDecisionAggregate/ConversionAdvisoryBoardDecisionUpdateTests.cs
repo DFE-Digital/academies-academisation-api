679aacb875ed6dc69818a39e776f28848f244d91
MartinWheelerMT <martin.wheeler@madetech.com>
2022-08-09 13:55:31 +0100

    AB#100708 - ConversionAdvisoryBoardDecision - Add missing unit tests for ConversionAdvisoryBoardDecision

---
+++ Dfe.Academies.Academisation.Domain.UnitTest/ConversionAdvisoryBoardDecisionAggregate/ConversionAdvisoryBoardDecisionUpdateTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Dfe.Academies.Academisation.Core;
+using Dfe.Academies.Academisation.Domain.ConversionAdvisoryBoardDecisionAggregate;
+using Dfe.Academies.Academisation.Domain.Core;
+using Xunit;
+
+namespace Dfe.Academies.Academisation.Domain.UnitTest.ConversionAdvisoryBoardDecisionAggregate;
+
+public class ConversionAdvisoryBoardDecisionUpdateTests
+{
+	[Fact]
+	public void DetailsAreValid___ReturnsCommandSuccessResult_DecisionMutated()
+	{
+		//Arrange 
+		var timestamp = DateTime.UtcNow;
+		
+		AdvisoryBoardDecisionDetails details = new(
+			default,
+			1,
+			AdvisoryBoardDecision.Approved,
+			false,
+			null,
+			null,
+			null,
+			null,
+			null,
+			timestamp.AddDays(-1),
+			DecisionMadeBy.DirectorGeneral);
+
+		var updatedDetails = details with {DecisionMadeBy = DecisionMadeBy.Minister};
+
+		ConversionAdvisoryBoardDecision expected = new(1, updatedDetails, timestamp, timestamp);
+		ConversionAdvisoryBoardDecision target = new(1, details, timestamp, timestamp);
+		
+		//Act
+		var result = target.Update(updatedDetails);
+
+		//Assert
+		Assert.Multiple(
+			() => Assert.IsType<CommandSuccessResult>(result),
+			() => Assert.Equivalent(expected, target)
+		);
+	}
+	
+	[Fact]
+    	public void DetailsAreInvalid___ReturnsCommandValidationErrorResult_DecisionNotMutated()
+    	{
+    		//Arrange 
+    		var timestamp = DateTime.UtcNow;
+            
+    		AdvisoryBoardDecisionDetails details = new(
+    			default,
+    			1,
+    			AdvisoryBoardDecision.Approved,
+    			true,
+    			null,
+    			null,
+    			null,
+    			null,
+    			null,
+    			timestamp.AddDays(-1),
+    			DecisionMadeBy.DirectorGeneral);
+    
+    		var updatedDetails = details with {DecisionMadeBy = DecisionMadeBy.Minister};
+    
+    		ConversionAdvisoryBoardDecision expected = new(1, details, timestamp, timestamp);
+    		ConversionAdvisoryBoardDecision target = new(1, details, timestamp, timestamp);
+    		
+    		//Act
+    		var result = target.Update(updatedDetails);
+    
+    		//Assert
+    		Assert.Multiple(
+    			() => Assert.IsType<CommandValidationErrorResult>(result),
+    			() => Assert.Equivalent(expected, target)
+    		);
+    	}
+}
