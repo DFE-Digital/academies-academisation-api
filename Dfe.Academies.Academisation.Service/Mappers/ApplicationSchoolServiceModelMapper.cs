384e50a6acbae667417ab8c8da7f3bfe01bb835b
Catherine LAWLOR <Catherine.LAWLOR@education.gov.uk>
2022-08-10 11:02:38 +0100

    AB#92464 API Update Application - Get mapping schools

--- Dfe.Academies.Academisation.Service/Mappers/ApplicationSchoolServiceModelMapper.cs
+++ Dfe.Academies.Academisation.Service/Mappers/ApplicationSchoolServiceModelMapper.cs
@@ -11,7 +11,15 @@ internal static class ApplicationSchoolServiceModelMapper
 		return new(
 			school.Id,
 			school.Details.Urn
-		);
+		)
+		{ 
+			ProposedNewSchoolName = school.Details.ProposedNewSchoolName,
+			ProjectedPupilNumbersYear1 = school.Details.ProjectedPupilNumbersYear1,
+			ProjectedPupilNumbersYear2 = school.Details.ProjectedPupilNumbersYear2,
+			ProjectedPupilNumbersYear3 = school.Details.ProjectedPupilNumbersYear3,
+			SchoolCapacityAssumptions = school.Details.SchoolCapacityAssumptions,
+			SchoolCapacityPublishedAdmissionsNumber = school.Details.SchoolCapacityPublishedAdmissionsNumber
+		};
 	}
 
 	internal static SchoolDetails ToDomain(this ApplicationSchoolServiceModel serviceModel)
