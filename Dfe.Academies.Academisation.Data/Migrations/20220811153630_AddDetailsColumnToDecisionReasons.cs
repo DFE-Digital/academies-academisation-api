9c49f839536928d47141c175ec697dea41a8f680
MartinWheelerMT <martin.wheeler@madetech.com>
2022-08-11 17:20:40 +0100

    AB#103054 - AdvisoryBoardDecision - Add Migration for model changes

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220811153630_AddDetailsColumnToDecisionReasons.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class AddDetailsColumnToDecisionReasons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeclinedOtherReason",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecision");
+
+            migrationBuilder.DropColumn(
+                name: "DeferredOtherReason",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecision");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecisionDeferredReason",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecisionDeclinedReason",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecisionDeferredReason");
+
+            migrationBuilder.DropColumn(
+                name: "Details",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecisionDeclinedReason");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeclinedOtherReason",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecision",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeferredOtherReason",
+                schema: "academisation",
+                table: "ConversionAdvisoryBoardDecision",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
