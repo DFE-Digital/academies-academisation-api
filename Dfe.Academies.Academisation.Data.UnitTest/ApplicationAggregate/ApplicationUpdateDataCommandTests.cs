8e506e5acc05142b0f56b453672943e9daa3ac92
Wystan Palm <wystan.palm@madetech.com>
2022-08-23 18:39:50 +0100

    AB#104076 Create Date Overwritten - more unit tests

--- Dfe.Academies.Academisation.Data.UnitTest/ApplicationAggregate/ApplicationUpdateDataCommandTests.cs
+++ Dfe.Academies.Academisation.Data.UnitTest/ApplicationAggregate/ApplicationUpdateDataCommandTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
 using Dfe.Academies.Academisation.Data.ApplicationAggregate;
 using Dfe.Academies.Academisation.Data.UnitTest.Contexts;
+using Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate;
 using Dfe.Academies.Academisation.IDomain.ApplicationAggregate;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -14,78 +16,235 @@ namespace Dfe.Academies.Academisation.Data.UnitTest.ApplicationAggregate;
 public class ApplicationUpdateDataCommandTests
 {
 	private readonly AcademisationContext _context;
-
 	private readonly Fixture _fixture = new();
-	private readonly Mock<IApplication> _mockApplication = new();
+	private readonly ApplicationGetDataQuery _query;
+	private readonly ApplicationUpdateDataCommand _subject;
 
 	public ApplicationUpdateDataCommandTests()
 	{
 		_context = new TestApplicationContext().CreateContext();
+		_query = new ApplicationGetDataQuery(_context);
+		_subject = new ApplicationUpdateDataCommand(_context);
 	}
 
 	[Fact]
 	public async Task WhenRecordDoesNotExist___ThrowsApplicationException()
 	{
+		// Arrange
 		const int applicationId = 4;
 
-		ApplicationUpdateDataCommand subject = new(_context);
+		Mock<IApplication> mockApplication = new();
 
-		_mockApplication.SetupGet(a => a.ApplicationId).Returns(applicationId);
+		mockApplication.SetupGet(a => a.ApplicationId).Returns(applicationId);
 
-		_mockApplication.SetupGet(d => d.Contributors)
+		mockApplication.SetupGet(d => d.Contributors)
 		 	.Returns(new List<IContributor>());
 
-		_mockApplication.SetupGet(d => d.Schools)
+		mockApplication.SetupGet(d => d.Schools)
 		 	.Returns(new List<ISchool>());
 
 		//Act & Assert
-		await Assert.ThrowsAsync<ApplicationException>(() => subject.Execute(_mockApplication.Object));
+		await Assert.ThrowsAsync<ApplicationException>(() => _subject.Execute(mockApplication.Object));
 	}
 
 	[Fact]
-	public async Task WhenRecordAlreadyExists___UpdatesExistingRecord()
+	public async Task RecordAlreadyExists_NoChange___LastModifiedOnUpdated()
 	{
 		//Arrange
 		const int applicationId = 1;
 
-		ApplicationUpdateDataCommand subject = new(_context);
+		var existingApplication = await _query.Execute(applicationId);
+		Assert.NotNull(existingApplication);
 
-		var existingApplication = await _context.Applications
-			.AsNoTracking()
-			.SingleAsync(a => a.Id == applicationId);
+		Mock<IApplication> mockApplication = CloneAsMock(existingApplication);
 
-		_mockApplication
+		//Act
+		await _subject.Execute(mockApplication.Object);
+		_context.ChangeTracker.Clear();
+		var updatedApplication = await _query.Execute(applicationId);
+
+		//Assert
+		Assert.NotNull(updatedApplication);
+		Assert.Multiple(
+			() => Assert.Equal(existingApplication.CreatedOn, updatedApplication.CreatedOn),
+			() => Assert.NotEqual(existingApplication.LastModifiedOn, updatedApplication.LastModifiedOn)
+		);
+	}
+
+	[Fact]
+	public async Task RecordAlreadyExists_FieldsChanged___FieldChangesPersisted()
+	{
+		//Arrange
+		const int applicationId = 1;
+
+		var existingApplication = await _query.Execute(applicationId);
+		Assert.NotNull(existingApplication);
+
+		Mock<IApplication> mockApplication = CloneAsMock(existingApplication);
+
+		mockApplication.SetupGet(a => a.ApplicationStatus).Returns(_fixture.Create<ApplicationStatus>());
+		mockApplication.SetupGet(a => a.ApplicationType).Returns(_fixture.Create<ApplicationType>());
+
+		//Act
+		await _subject.Execute(mockApplication.Object);
+		_context.ChangeTracker.Clear();
+		var updatedApplication = await _query.Execute(applicationId);
+
+		//Assert
+		Assert.NotNull(updatedApplication);
+		Assert.Multiple(
+			() => Assert.Equal(existingApplication.ApplicationStatus, updatedApplication.ApplicationStatus),
+			() => Assert.Equal(existingApplication.ApplicationType, updatedApplication.ApplicationType)
+		);
+	}
+
+	[Fact]
+	public async Task RecordAlreadyExists_ContributorAdded___AddedContributorPersisted()
+	{
+		//Arrange
+		const int applicationId = 1;
+
+		var existingApplication = await _query.Execute(applicationId);
+		Assert.NotNull(existingApplication);
+
+		Mock<IApplication> mockApplication = CloneAsMock(existingApplication);
+
+		var addedContributorDetails = _fixture.Create<ContributorDetails>();
+		var addedContributor = new Mock<IContributor>();
+		addedContributor.SetupGet(c => c.Details).Returns(addedContributorDetails);
+
+		mockApplication.SetupGet(d => d.Contributors)
+		 	.Returns(new List<IContributor>(existingApplication.Contributors)
+			{
+				addedContributor.Object
+			});
+
+		//Act
+		await _subject.Execute(mockApplication.Object);
+		_context.ChangeTracker.Clear();
+		var updatedApplication = await _query.Execute(applicationId);
+
+		//Assert
+		Assert.NotNull(updatedApplication);
+		Assert.Contains(addedContributorDetails, updatedApplication.Contributors.Select(c => c.Details));
+	}
+
+	[Fact]
+	public async Task RecordAlreadyExists_ContributorRemoved___RemovedContributorPersisted()
+	{
+		//Arrange
+		const int applicationId = 1;
+
+		var existingApplication = await _query.Execute(applicationId);
+		Assert.NotNull(existingApplication);
+
+		Mock<IApplication> mockApplication = CloneAsMock(existingApplication);
+
+		var mutatedContributorList = new List<IContributor>(existingApplication.Contributors);
+		var contributorToRemove = PickRandomElement(mutatedContributorList);
+		mutatedContributorList.Remove(contributorToRemove);
+
+		mockApplication.SetupGet(d => d.Contributors)
+		 	.Returns(mutatedContributorList);
+
+		//Act
+		await _subject.Execute(mockApplication.Object);
+		_context.ChangeTracker.Clear();
+		var updatedApplication = await _query.Execute(applicationId);
+
+		//Assert
+		Assert.NotNull(updatedApplication);
+		Assert.DoesNotContain(contributorToRemove.Details, updatedApplication.Contributors.Select(c => c.Details));
+	}
+
+	[Fact]
+	public async Task RecordAlreadyExists_SchoolAdded___AddedSchoolPersisted()
+	{
+		//Arrange
+		const int applicationId = 1;
+
+		var existingApplication = await _query.Execute(applicationId);
+		Assert.NotNull(existingApplication);
+
+		Mock<IApplication> mockApplication = CloneAsMock(existingApplication);
+
+		var addedSchoolDetails = _fixture.Create<SchoolDetails>();
+		var addedSchool = new Mock<ISchool>();
+		addedSchool.SetupGet(c => c.Details).Returns(addedSchoolDetails);
+
+		mockApplication.SetupGet(d => d.Schools)
+		 	.Returns(new List<ISchool>(existingApplication.Schools)
+			{
+				addedSchool.Object
+			});
+
+		//Act
+		await _subject.Execute(mockApplication.Object);
+		_context.ChangeTracker.Clear();
+		var updatedApplication = await _query.Execute(applicationId);
+
+		//Assert
+		Assert.NotNull(updatedApplication);
+		Assert.Contains(addedSchoolDetails, updatedApplication.Schools.Select(s => s.Details));
+	}
+
+	[Fact]
+	public async Task RecordAlreadyExists_SchoolRemoved___RemovedSchoolPersisted()
+	{
+		//Arrange
+		const int applicationId = 1;
+
+		var existingApplication = await _query.Execute(applicationId);
+		Assert.NotNull(existingApplication);
+
+		Mock<IApplication> mockApplication = CloneAsMock(existingApplication);
+
+		var mutatedSchoolList = new List<ISchool>(existingApplication.Schools);
+		var contributorToRemove = PickRandomElement(mutatedSchoolList);
+		mutatedSchoolList.Remove(contributorToRemove);
+
+		mockApplication.SetupGet(d => d.Schools)
+		 	.Returns(mutatedSchoolList);
+
+		//Act
+		await _subject.Execute(mockApplication.Object);
+		_context.ChangeTracker.Clear();
+		var updatedApplication = await _query.Execute(applicationId);
+
+		//Assert
+		Assert.NotNull(updatedApplication);
+		Assert.DoesNotContain(contributorToRemove.Details, updatedApplication.Schools.Select(s => s.Details));
+	}
+
+	private static Mock<IApplication> CloneAsMock(IApplication existingApplication)
+	{
+		Mock<IApplication> mockApplication = new();
+
+		mockApplication
 			.SetupGet(a => a.ApplicationId)
-			.Returns(applicationId);
+			.Returns(existingApplication.ApplicationId);
 
-		_mockApplication
+		mockApplication
 			.SetupGet(a => a.CreatedOn)
 			.Returns(existingApplication.CreatedOn);
 
-		_mockApplication
+		mockApplication
 			.SetupGet(a => a.LastModifiedOn)
 			.Returns(existingApplication.LastModifiedOn);
 
-		_mockApplication.SetupGet(d => d.Contributors)
-		 	.Returns(new List<IContributor>());
+		mockApplication.SetupGet(d => d.Contributors)
+			 .Returns(existingApplication.Contributors);
 
-		_mockApplication.SetupGet(d => d.Schools)
-		 	.Returns(new List<ISchool>());
+		mockApplication.SetupGet(d => d.Schools)
+			.Returns(existingApplication.Schools);
 
-		await _context.Applications.LoadAsync();
+		return mockApplication;
+	}
 
-		//Act
-		await subject.Execute(_mockApplication.Object);
-
-		_context.ChangeTracker.Clear();
-
-		var queryResult = await _context.Applications.FindAsync(applicationId);
-
-		//Assert
-		Assert.Multiple(
-			//() => Assert.NotEqual(existingApplication.ApprovedConditionsSet, result!.ApprovedConditionsSet),
-			() => Assert.NotEqual(existingApplication.LastModifiedOn, queryResult!.LastModifiedOn),
-			() => Assert.Equal(existingApplication.CreatedOn, queryResult!.CreatedOn)
-		);
+	private static T PickRandomElement<T>(IEnumerable<T> enumerable)
+	{
+		Random random = new();
+		var list = enumerable.ToList();
+		return list.ElementAt(random.Next(1, list.Count));
 	}
 }
