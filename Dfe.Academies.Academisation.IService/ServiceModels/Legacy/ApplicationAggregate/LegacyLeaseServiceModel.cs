c8eee448f616f7674ab77093f09c13a713d20e30
Wystan Palm <wystan.palm@madetech.com>
2022-08-25 09:48:59 +0100

    AB#104570 API Legacy Application Get - service models

---
+++ Dfe.Academies.Academisation.IService/ServiceModels/Legacy/ApplicationAggregate/LegacyLeaseServiceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfe.Academies.Academisation.IService.ServiceModels.Legacy.ApplicationAggregate;
+
+public record LegacyLeaseServiceModel(
+	decimal SchoolLeaseRepaymentValue,
+	decimal SchoolLeaseInterestRate,
+	decimal SchoolLeasePaymentToDate,
+	string? SchoolLeaseTerm = null,
+	string? SchoolLeasePurpose = null,
+	string? SchoolLeaseValueOfAssets = null,
+	string? SchoolLeaseResponsibleForAssets = null
+);
