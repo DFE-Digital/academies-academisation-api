1859f2109c752d4ac368c4fd81602bb00da25c23
Wystan Palm <wystan.palm@madetech.com>
2022-09-02 16:50:22 +0100

    AB#104570 Legacy Application Get - mapping, schools and key persons

--- Dfe.Academies.Academisation.Service/Mappers/Legacy/ApplicationAggregate/LegacyKeyPersonServiceModelMapper.cs
+++ Dfe.Academies.Academisation.Service/Mappers/Legacy/ApplicationAggregate/LegacyKeyPersonServiceModelMapper.cs
@@ -1,4 +1,5 @@
-﻿using Dfe.Academies.Academisation.IDomain.ApplicationAggregate;
+﻿using Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate;
+using Dfe.Academies.Academisation.IDomain.ApplicationAggregate;
 using Dfe.Academies.Academisation.IService.ServiceModels.Legacy.ApplicationAggregate;
 
 namespace Dfe.Academies.Academisation.Service.Mappers.Legacy.ApplicationAggregate;
@@ -7,6 +8,14 @@ internal static class LegacyKeyPersonServiceModelMapper
 {
 	internal static ICollection<LegacyKeyPersonServiceModel> MapToServiceModels(this IApplication application)
 	{
+		if (application.ApplicationType == ApplicationType.JoinAMat)
+		{
+			// Key Persons only applies when forming a trust
+			return new List<LegacyKeyPersonServiceModel>();
+		}
+
+		// For FormAMat and FormASat it would be necessary to map this from the NewTrust object
+		// (but it's probably not worth implementing them in this legacy service model)
 		throw new NotImplementedException();
 	}
 }
