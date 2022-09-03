0122004557bcfa301cb10284993f637cabd2146a
Wystan Palm <wystan.palm@madetech.com>
2022-09-04 00:25:27 +0100

    AB#104572 API Get Project - domain model

---
+++ Dfe.Academies.Academisation.Domain/ProjectAggregate/Project.cs
@@ -0,0 +1,24 @@
+﻿using Dfe.Academies.Academisation.Domain.Core.ProjectAggregate;
+
+namespace Dfe.Academies.Academisation.Domain.ProjectAggregate;
+
+public class Project
+{
+	private Project(ProjectDetails projectDetails)
+	{
+		ProjectDetails = projectDetails;
+	}
+
+	/// <summary>
+	/// This is the persistence constructor, only use from the data layer
+	/// </summary>
+	public Project(int id, ProjectDetails projectDetails)
+	{
+		Id = id;
+		ProjectDetails = projectDetails;
+	}
+
+	public int Id { get; }
+
+	public ProjectDetails ProjectDetails { get; }
+}
---
+++ Dfe.Academies.Academisation.IDomain/ProjectAggregate/IProject.cs
@@ -0,0 +1,10 @@
+﻿using Dfe.Academies.Academisation.Domain.Core.ProjectAggregate;
+
+namespace Dfe.Academies.Academisation.IDomain.ProjectAggregate;
+
+public interface IProject
+{
+	public int Id { get; }
+
+	public ProjectDetails ProjectDetails { get; }
+}
