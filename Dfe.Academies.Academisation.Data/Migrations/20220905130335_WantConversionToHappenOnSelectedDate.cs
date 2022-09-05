77b245527cd13612674c185818b0bc2de5e0b8d0
mark robinson <mark.robinson@education.gov.uk>
2022-09-05 14:05:05 +0100

    AB#105435 - conversion target date - store whether target date has been chosen - new migration for new DB column

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220905130335_WantConversionToHappenOnSelectedDate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class WantConversionToHappenOnSelectedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "WantConversionToHappenOnSelectedDate",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WantConversionToHappenOnSelectedDate",
+                schema: "academisation",
+                table: "ApplicationSchool");
+        }
+    }
+}
