3413488e37b815fc1e1db248cb11f193ee22c78c
Wystan Palm <wystan.palm@madetech.com>
2022-09-06 14:47:58 +0100

    AB#104572 API Get Project - PR fix compiler warning

--- Dfe.Academies.Academisation.IService/Query/ILegacyProjectGetQuery.cs
+++ Dfe.Academies.Academisation.IService/Query/ILegacyProjectGetQuery.cs
@@ -4,5 +4,5 @@ namespace Dfe.Academies.Academisation.IService.Query;
 
 public interface ILegacyProjectGetQuery
 {
-	Task<LegacyProjectServiceModel> Execute(int id);
+	Task<LegacyProjectServiceModel?> Execute(int id);
 }
