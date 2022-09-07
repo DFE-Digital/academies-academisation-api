0e31699c8e94f68411e80f7f6f32b7c2f69a9f37
mark robinson <mark.robinson@education.gov.uk>
2022-09-07 11:50:23 +0100

    AB#105435 - conversion target date - store whether target date has been chosen - added property & mapping

--- Dfe.Academies.Academisation.Service/Mappers/Application/ApplicationSchoolServiceModelMapper.cs
+++ Dfe.Academies.Academisation.Service/Mappers/Application/ApplicationSchoolServiceModelMapper.cs
@@ -59,6 +59,7 @@ internal static class ApplicationSchoolServiceModelMapper
 			ContactChairTel = serviceModel.SchoolConversionContactChairTel,
 			ContactRole = serviceModel.SchoolConversionContactRole,
 			ApplicationJoinTrustReason = serviceModel.ApplicationJoinTrustReason,
+			ConversionTargetDateSpecified = serviceModel.SchoolConversionTargetDateSpecified,
 			ConversionTargetDate = serviceModel.SchoolConversionTargetDate,
 			ConversionTargetDateExplained = serviceModel.SchoolConversionTargetDateExplained,
 			ProposedNewSchoolName = serviceModel.ProposedNewSchoolName,
