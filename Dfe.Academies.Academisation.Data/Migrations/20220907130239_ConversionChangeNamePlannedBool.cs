a6ecde93e2a74a8f7b95f3407907b4e391542edd
mark robinson <mark.robinson@education.gov.uk>
2022-09-07 14:03:24 +0100

    AB#105758 - school - missing bools - EF migration for new col

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220907130239_ConversionChangeNamePlannedBool.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class ConversionChangeNamePlannedBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConversionChangeNamePlanned",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConversionChangeNamePlanned",
+                schema: "academisation",
+                table: "ApplicationSchool");
+        }
+    }
+}
