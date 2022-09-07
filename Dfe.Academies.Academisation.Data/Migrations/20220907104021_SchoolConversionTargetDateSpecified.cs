2013b11170f245d72967452a307e706bda76eb24
mark robinson <mark.robinson@education.gov.uk>
2022-09-07 11:40:41 +0100

    AB#105435 - conversion target date - store whether target date has been chosen - new EF migration for new DB column

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220907104021_SchoolConversionTargetDateSpecified.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class SchoolConversionTargetDateSpecified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConversionTargetDateSpecified",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConversionTargetDateSpecified",
+                schema: "academisation",
+                table: "ApplicationSchool");
+        }
+    }
+}
