14338d9c9a2973578a497ae7289d15b076981278
mark robinson <mark.robinson@education.gov.uk>
2022-09-08 10:29:25 +0100

    AB#105464 - API - Pre-support grant - EF migration to create new database columns

---
+++ Dfe.Academies.Academisation.Data/Migrations/20220908092640_ApplicationPreSupportGrant.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class ApplicationPreSupportGrant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfirmPaySupportGrantToSchool",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupportGrantFundsPaidTo",
+                schema: "academisation",
+                table: "ApplicationSchool",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPaySupportGrantToSchool",
+                schema: "academisation",
+                table: "ApplicationSchool");
+
+            migrationBuilder.DropColumn(
+                name: "SupportGrantFundsPaidTo",
+                schema: "academisation",
+                table: "ApplicationSchool");
+        }
+    }
+}
