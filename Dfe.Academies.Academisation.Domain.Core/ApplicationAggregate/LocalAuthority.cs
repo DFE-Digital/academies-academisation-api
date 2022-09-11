d8346146212f65849fc4b81d0ce452df364dcb0c
mark robinson <mark.robinson@education.gov.uk>
2022-09-11 16:31:05 +0100

    AB#105888 - API - School Additional Information - added another object to split out LA information

---
+++ Dfe.Academies.Academisation.Domain.Core/ApplicationAggregate/LocalAuthority.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate
+{
+	public record LocalAuthority(
+		bool? PartOfLaReorganizationPlan = null,
+		string? LaReorganizationDetails = null,
+		bool? PartOfLaClosurePlan = null,
+		string? LaClosurePlanDetails = null
+		);
+}
---
+++ Dfe.Academies.Academisation.IService/ServiceModels/Application/LocalAuthorityServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.Academies.Academisation.IService.ServiceModels.Application
+{
+	public record LocalAuthorityServiceModel(
+		bool? PartOfLaReorganizationPlan = null,
+		string? LaReorganizationDetails = null,
+		bool? PartOfLaClosurePlan = null,
+		string? LaClosurePlanDetails = null
+		);
+}
