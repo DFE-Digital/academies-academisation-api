200e8835b20d0c7e91b5c72fd588eaadf48acec6
Wystan Palm <wystan.palm@madetech.com>
2022-09-12 12:09:16 +0100

    AB#104571 Submit Application Create Project - JoinAMat submit validation - one school

--- Dfe.Academies.Academisation.Domain.UnitTest/ApplicationAggregate/ApplicationSubmitTests.cs
+++ Dfe.Academies.Academisation.Domain.UnitTest/ApplicationAggregate/ApplicationSubmitTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using AutoFixture;
 using Dfe.Academies.Academisation.Core;
+using Dfe.Academies.Academisation.Core.Test;
 using Dfe.Academies.Academisation.Domain.ApplicationAggregate;
 using Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate;
 using Xunit;
@@ -9,6 +11,8 @@ namespace Dfe.Academies.Academisation.Domain.UnitTest.ApplicationAggregate
 {
 	public class ApplicationSubmitTests
 	{
+		private readonly Fixture _fixture = new();
+
 		[Fact]
 		public void ApplicationStatusIsSubmitted___ValidationError()
 		{
@@ -49,5 +53,55 @@ namespace Dfe.Academies.Academisation.Domain.UnitTest.ApplicationAggregate
 			Assert.IsType<CommandSuccessResult>(result);
 			Assert.Equal(ApplicationStatus.Submitted, subject.ApplicationStatus);
 		}
+
+		[Fact]
+		public void ApplicationTypeIsJoinAMat_MoreThanOneSchool___ValidationError()
+		{
+			// arrange
+			Application subject = new(
+				1,
+				DateTime.UtcNow,
+				DateTime.UtcNow,
+				ApplicationType.JoinAMat,
+				ApplicationStatus.InProgress,
+				new Dictionary<int, ContributorDetails>(),
+				new Dictionary<int, SchoolDetails>
+				{
+					{ 1, _fixture.Create<SchoolDetails>() },
+					{ 2, _fixture.Create<SchoolDetails>() }
+				});
+
+			// act
+			var result = subject.Submit();
+
+			// assert
+			DfeAssert.CommandValidationError(result, nameof(Application.Schools));
+		}
+
+
+		[Fact]
+		public void ApplicationTypeIsJoinAMat_OneSchool___Success()
+		{
+			// arrange
+			Application subject = new(
+				1,
+				DateTime.UtcNow,
+				DateTime.UtcNow,
+				ApplicationType.JoinAMat,
+				ApplicationStatus.InProgress,
+				new Dictionary<int, ContributorDetails>(),
+				new Dictionary<int, SchoolDetails>
+				{
+					{ 1, _fixture.Create<SchoolDetails>() },
+				});
+
+			// act
+			var result = subject.Submit();
+
+			// assert
+			Assert.IsType<CommandSuccessResult>(result);
+			Assert.Equal(ApplicationStatus.Submitted, subject.ApplicationStatus);
+		}
+
 	}
 }
