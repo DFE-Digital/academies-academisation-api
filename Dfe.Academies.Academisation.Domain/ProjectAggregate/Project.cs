b84f99be4e4f05becb4dbb0aa8d47990e21ec3dd
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-09-12 17:27:13 +0100

    AB#104571 Submit Application Create Project - mapping patched project details.

--- Dfe.Academies.Academisation.Domain/ProjectAggregate/Project.cs
+++ Dfe.Academies.Academisation.Domain/ProjectAggregate/Project.cs
@@ -43,7 +43,7 @@ public class Project : IProject
 			school.Urn,
 			misEstablishmentDetails.Laestab
 		)
-		{ 
+		{
 			UkPrn = establishmentDetails.Ukprn,
 			//LocalAuthority = school.LocalAuthority.LocalAuthorityName,
 			//ApplicationReferenceNumber = application.ApplicationId
@@ -68,10 +68,85 @@ public class Project : IProject
 			//CapitalCarryForwardAtEndMarchNextYear = school.SchoolNFYCapitalForward.ConvertDeficitAmountToNegativeValue(school.SchoolNFYCapitalIsDeficit),
 			YearOneProjectedPupilNumbers = school.ProjectedPupilNumbersYear1,
 			YearTwoProjectedPupilNumbers = school.ProjectedPupilNumbersYear2,
-			YearThreeProjectedPupilNumbers	= school.ProjectedPupilNumbersYear3
+			YearThreeProjectedPupilNumbers = school.ProjectedPupilNumbersYear3
 		};
 
 		return new CreateSuccessResult<IProject>(new Project(projectDetails));
+	}
+
+	public CommandResult UpdatePatch(ProjectDetails detailsToUpdate)
+	{
+		var patchedDetails = new ProjectDetails(1, 1)
+		{
+			HeadTeacherBoardDate = Details.HeadTeacherBoardDate == default(DateTime)
+			   ? null
+			   : detailsToUpdate.HeadTeacherBoardDate ?? Details.HeadTeacherBoardDate,
+			Author = detailsToUpdate.Author ?? Details.Author,
+			ClearedBy = detailsToUpdate.ClearedBy ?? Details.ClearedBy,
+			ProposedAcademyOpeningDate = detailsToUpdate.ProposedAcademyOpeningDate ?? Details.ProposedAcademyOpeningDate,
+			PublishedAdmissionNumber = detailsToUpdate.PublishedAdmissionNumber ?? Details.PublishedAdmissionNumber,
+			ViabilityIssues = detailsToUpdate.ViabilityIssues ?? Details.ViabilityIssues,
+			FinancialDeficit = detailsToUpdate.FinancialDeficit ?? Details.FinancialDeficit,
+			RationaleForProject = detailsToUpdate.RationaleForProject ?? Details.RationaleForProject,
+			RationaleForTrust = detailsToUpdate.RationaleForTrust ?? Details.RationaleForTrust,
+			RisksAndIssues = detailsToUpdate.RisksAndIssues ?? Details.RisksAndIssues,
+			RevenueCarryForwardAtEndMarchCurrentYear = detailsToUpdate.RevenueCarryForwardAtEndMarchCurrentYear == default(decimal)
+				? null
+				: detailsToUpdate.RevenueCarryForwardAtEndMarchCurrentYear ?? Details.RevenueCarryForwardAtEndMarchCurrentYear,
+			ProjectedRevenueBalanceAtEndMarchNextYear = detailsToUpdate.ProjectedRevenueBalanceAtEndMarchNextYear == default(decimal)
+				? null
+				: detailsToUpdate.ProjectedRevenueBalanceAtEndMarchNextYear ?? Details.ProjectedRevenueBalanceAtEndMarchNextYear,
+			RationaleSectionComplete = detailsToUpdate.RationaleSectionComplete ?? Details.RationaleSectionComplete,
+			LocalAuthorityInformationTemplateSentDate = detailsToUpdate.LocalAuthorityInformationTemplateSentDate == default(DateTime)
+				? null
+				: detailsToUpdate.LocalAuthorityInformationTemplateSentDate ?? Details.LocalAuthorityInformationTemplateSentDate,
+			LocalAuthorityInformationTemplateReturnedDate = detailsToUpdate.LocalAuthorityInformationTemplateReturnedDate == default(DateTime)
+				? null
+				: detailsToUpdate.LocalAuthorityInformationTemplateReturnedDate ?? Details.LocalAuthorityInformationTemplateReturnedDate,
+			LocalAuthorityInformationTemplateComments = detailsToUpdate.LocalAuthorityInformationTemplateComments ?? Details.LocalAuthorityInformationTemplateComments,
+			LocalAuthorityInformationTemplateLink = detailsToUpdate.LocalAuthorityInformationTemplateLink ?? Details.LocalAuthorityInformationTemplateLink,
+			LocalAuthorityInformationTemplateSectionComplete = detailsToUpdate.LocalAuthorityInformationTemplateSectionComplete ?? Details.LocalAuthorityInformationTemplateSectionComplete,
+			RecommendationForProject = detailsToUpdate.RecommendationForProject ?? Details.RecommendationForProject,
+			AcademyOrderRequired = detailsToUpdate.AcademyOrderRequired ?? Details.AcademyOrderRequired,
+			SchoolAndTrustInformationSectionComplete = detailsToUpdate.SchoolAndTrustInformationSectionComplete ?? Details.SchoolAndTrustInformationSectionComplete,
+			DistanceFromSchoolToTrustHeadquarters = detailsToUpdate.DistanceFromSchoolToTrustHeadquarters == default(decimal)
+				? null
+				: detailsToUpdate.DistanceFromSchoolToTrustHeadquarters ?? Details.DistanceFromSchoolToTrustHeadquarters,
+			DistanceFromSchoolToTrustHeadquartersAdditionalInformation = detailsToUpdate.DistanceFromSchoolToTrustHeadquartersAdditionalInformation ?? Details.DistanceFromSchoolToTrustHeadquartersAdditionalInformation,
+			MemberOfParliamentName = detailsToUpdate.MemberOfParliamentName ?? Details.MemberOfParliamentName,
+			MemberOfParliamentParty = detailsToUpdate.MemberOfParliamentParty ?? Details.MemberOfParliamentParty,
+			GeneralInformationSectionComplete = detailsToUpdate.GeneralInformationSectionComplete ?? Details.GeneralInformationSectionComplete,
+			RisksAndIssuesSectionComplete = detailsToUpdate.RisksAndIssuesSectionComplete ?? Details.RisksAndIssuesSectionComplete,
+			SchoolPerformanceAdditionalInformation = detailsToUpdate.SchoolPerformanceAdditionalInformation ?? Details.SchoolPerformanceAdditionalInformation,
+			CapitalCarryForwardAtEndMarchCurrentYear = detailsToUpdate.CapitalCarryForwardAtEndMarchCurrentYear == default(decimal)
+				? null
+				: detailsToUpdate.CapitalCarryForwardAtEndMarchCurrentYear ?? Details.CapitalCarryForwardAtEndMarchCurrentYear,
+			CapitalCarryForwardAtEndMarchNextYear = detailsToUpdate.CapitalCarryForwardAtEndMarchNextYear == default(decimal)
+				? null
+				: detailsToUpdate.CapitalCarryForwardAtEndMarchNextYear ?? Details.CapitalCarryForwardAtEndMarchNextYear,
+			SchoolBudgetInformationAdditionalInformation = detailsToUpdate.SchoolBudgetInformationAdditionalInformation ?? Details.SchoolBudgetInformationAdditionalInformation,
+			SchoolBudgetInformationSectionComplete = detailsToUpdate.SchoolBudgetInformationSectionComplete ?? Details.SchoolBudgetInformationSectionComplete,
+			SchoolPupilForecastsAdditionalInformation = detailsToUpdate.SchoolPupilForecastsAdditionalInformation ?? Details.SchoolPupilForecastsAdditionalInformation,
+			YearOneProjectedCapacity = detailsToUpdate.YearOneProjectedCapacity ?? Details.YearOneProjectedCapacity,
+			YearOneProjectedPupilNumbers = detailsToUpdate.YearOneProjectedPupilNumbers ?? Details.YearOneProjectedPupilNumbers,
+			YearTwoProjectedCapacity = detailsToUpdate.YearTwoProjectedCapacity ?? Details.YearTwoProjectedCapacity,
+			YearTwoProjectedPupilNumbers = detailsToUpdate.YearTwoProjectedPupilNumbers ?? Details.YearTwoProjectedPupilNumbers,
+			YearThreeProjectedCapacity = detailsToUpdate.YearThreeProjectedCapacity ?? Details.YearThreeProjectedCapacity,
+			YearThreeProjectedPupilNumbers = detailsToUpdate.YearThreeProjectedPupilNumbers ?? Details.YearThreeProjectedPupilNumbers,
+			KeyStage2PerformanceAdditionalInformation = detailsToUpdate.KeyStage2PerformanceAdditionalInformation ??
+			Details.KeyStage2PerformanceAdditionalInformation,
+			KeyStage4PerformanceAdditionalInformation = detailsToUpdate.KeyStage4PerformanceAdditionalInformation ?? Details.KeyStage4PerformanceAdditionalInformation,
+			KeyStage5PerformanceAdditionalInformation = detailsToUpdate.KeyStage5PerformanceAdditionalInformation ?? Details.KeyStage5PerformanceAdditionalInformation,
+			PreviousHeadTeacherBoardDateQuestion = detailsToUpdate.PreviousHeadTeacherBoardDateQuestion ?? Details.PreviousHeadTeacherBoardDateQuestion,
+			PreviousHeadTeacherBoardDate = detailsToUpdate.PreviousHeadTeacherBoardDate == default(DateTime)
+				? null
+				: detailsToUpdate.PreviousHeadTeacherBoardDate ?? Details.PreviousHeadTeacherBoardDate,
+			ConversionSupportGrantAmount = detailsToUpdate.ConversionSupportGrantAmount ?? Details.ConversionSupportGrantAmount,
+			ConversionSupportGrantChangeReason = detailsToUpdate.ConversionSupportGrantChangeReason ?? Details.ConversionSupportGrantChangeReason,
+			ProjectStatus = detailsToUpdate.ProjectStatus ?? Details.ProjectStatus
+		};		
+
+		return new CommandSuccessResult();
 	}
 
 	private static string? ToYesNoString(bool? value)
