3e4ccb560a4ce1a1314d4327bdf57605af3b5224
mark robinson <mark.robinson@education.gov.uk>
2022-09-12 14:04:51 +0100

    AB#105469 - API - School Finances - Current financial year - fleshed out after checking legacy model vs UI

--- Dfe.Academies.Academisation.Domain.Core/ApplicationAggregate/FinancialYear.cs
+++ Dfe.Academies.Academisation.Domain.Core/ApplicationAggregate/FinancialYear.cs
@@ -1,6 +1,14 @@
 ﻿namespace Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate
 {
 	public record FinancialYear(
-		// TODO MR:-
-		);
+		DateTime? FinancialYearEndDate = null,
+		decimal? Revenue = null,
+		RevenueType? RevenueStatus = null,
+		string? RevenueStatusExplained = null,
+		string? RevenueStatusFileLink = null,
+		decimal? CapitalCarryForward = null,
+		RevenueType? CapitalCarryForwardStatus = null,
+		string? CapitalCarryForwardExplained = null,
+		string? CapitalCarryForwardFileLink = null
+	);
 }
--- Dfe.Academies.Academisation.IService/ServiceModels/Application/FinancialYearServiceModel.cs
+++ Dfe.Academies.Academisation.IService/ServiceModels/Application/FinancialYearServiceModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Dfe.Academies.Academisation.IService.ServiceModels.Application
+﻿using Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate;
+
+namespace Dfe.Academies.Academisation.IService.ServiceModels.Application
 {
 	public record FinancialYearServiceModel(
-		// TODO MR:-
+		DateTime? FinancialYearEndDate = null,
+		decimal? Revenue = null,
+		RevenueType? RevenueStatus = null,
+		string? RevenueStatusExplained = null,
+		string? RevenueStatusFileLink = null,
+		decimal? CapitalCarryForward = null,
+		RevenueType? CapitalCarryForwardStatus = null,
+		string? CapitalCarryForwardExplained = null,
+		string? CapitalCarryForwardFileLink = null
 		);
 }
