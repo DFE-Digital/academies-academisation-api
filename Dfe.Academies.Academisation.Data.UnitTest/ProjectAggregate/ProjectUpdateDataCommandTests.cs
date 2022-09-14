34beac926ea2dd2124ee7b70764532f2b5f9bb2b
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-09-14 12:06:17 +0100

    AB#104574 Update Project - improve project assert for unit tests

--- Dfe.Academies.Academisation.Data.UnitTest/ProjectAggregate/ProjectUpdateDataCommandTests.cs
+++ Dfe.Academies.Academisation.Data.UnitTest/ProjectAggregate/ProjectUpdateDataCommandTests.cs
@@ -40,58 +40,6 @@ public class ProjectUpdateDataCommandTests
 		var updatedProject = await _context.Projects.SingleAsync(p => p.Id == project.Id);
 
 		// assert
-		AssertProjectModelIsEqual(project.Details, updatedProject);
-	}
-
-	private static void AssertProjectModelIsEqual(ProjectDetails expectedProject, ProjectState actualProject)
-	{
-		Assert.Multiple(
-					() => Assert.Equal(expectedProject.HeadTeacherBoardDate, actualProject.HeadTeacherBoardDate),
-					() => Assert.Equal(expectedProject.Author, actualProject.Author),
-					() => Assert.Equal(expectedProject.ClearedBy, actualProject.ClearedBy),
-					() => Assert.Equal(expectedProject.ProposedAcademyOpeningDate, actualProject.ProposedAcademyOpeningDate),
-					() => Assert.Equal(expectedProject.PublishedAdmissionNumber, actualProject.PublishedAdmissionNumber),
-					() => Assert.Equal(expectedProject.ViabilityIssues, actualProject.ViabilityIssues),
-					() => Assert.Equal(expectedProject.FinancialDeficit, actualProject.FinancialDeficit),
-					() => Assert.Equal(expectedProject.RationaleForProject, actualProject.RationaleForProject),
-					() => Assert.Equal(expectedProject.RisksAndIssues, actualProject.RisksAndIssues),
-					() => Assert.Equal(expectedProject.RevenueCarryForwardAtEndMarchCurrentYear, actualProject.RevenueCarryForwardAtEndMarchCurrentYear),
-					() => Assert.Equal(expectedProject.ProjectedRevenueBalanceAtEndMarchNextYear, actualProject.ProjectedRevenueBalanceAtEndMarchNextYear),
-					() => Assert.Equal(expectedProject.RationaleSectionComplete, actualProject.RationaleSectionComplete),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateSentDate, actualProject.LocalAuthorityInformationTemplateSentDate),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateReturnedDate, actualProject.LocalAuthorityInformationTemplateReturnedDate),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateComments, actualProject.LocalAuthorityInformationTemplateComments),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateLink, actualProject.LocalAuthorityInformationTemplateLink),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateSectionComplete, actualProject.LocalAuthorityInformationTemplateSectionComplete),
-					() => Assert.Equal(expectedProject.RecommendationForProject, actualProject.RecommendationForProject),
-					() => Assert.Equal(expectedProject.AcademyOrderRequired, actualProject.AcademyOrderRequired),
-					() => Assert.Equal(expectedProject.SchoolAndTrustInformationSectionComplete, actualProject.SchoolAndTrustInformationSectionComplete),
-					() => Assert.Equal(expectedProject.DistanceFromSchoolToTrustHeadquarters, actualProject.DistanceFromSchoolToTrustHeadquarters),
-					() => Assert.Equal(expectedProject.DistanceFromSchoolToTrustHeadquarters, actualProject.DistanceFromSchoolToTrustHeadquarters),
-					() => Assert.Equal(expectedProject.DistanceFromSchoolToTrustHeadquartersAdditionalInformation, actualProject.DistanceFromSchoolToTrustHeadquartersAdditionalInformation),
-					() => Assert.Equal(expectedProject.MemberOfParliamentName, actualProject.MemberOfParliamentName),
-					() => Assert.Equal(expectedProject.MemberOfParliamentParty, actualProject.MemberOfParliamentParty),
-					() => Assert.Equal(expectedProject.GeneralInformationSectionComplete, actualProject.GeneralInformationSectionComplete),
-					() => Assert.Equal(expectedProject.RisksAndIssuesSectionComplete, actualProject.RisksAndIssuesSectionComplete),
-					() => Assert.Equal(expectedProject.SchoolPerformanceAdditionalInformation, actualProject.SchoolPerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.CapitalCarryForwardAtEndMarchCurrentYear, actualProject.CapitalCarryForwardAtEndMarchCurrentYear),
-					() => Assert.Equal(expectedProject.CapitalCarryForwardAtEndMarchNextYear, actualProject.CapitalCarryForwardAtEndMarchNextYear),
-					() => Assert.Equal(expectedProject.SchoolBudgetInformationAdditionalInformation, actualProject.SchoolBudgetInformationAdditionalInformation),
-					() => Assert.Equal(expectedProject.SchoolAndTrustInformationSectionComplete, actualProject.SchoolAndTrustInformationSectionComplete),
-					() => Assert.Equal(expectedProject.SchoolPupilForecastsAdditionalInformation, actualProject.SchoolPupilForecastsAdditionalInformation),
-					() => Assert.Equal(expectedProject.YearOneProjectedCapacity, actualProject.YearOneProjectedCapacity),
-					() => Assert.Equal(expectedProject.YearOneProjectedPupilNumbers, actualProject.YearOneProjectedPupilNumbers),
-					() => Assert.Equal(expectedProject.YearTwoProjectedCapacity, actualProject.YearTwoProjectedCapacity),
-					() => Assert.Equal(expectedProject.YearTwoProjectedPupilNumbers, actualProject.YearTwoProjectedPupilNumbers),
-					() => Assert.Equal(expectedProject.YearThreeProjectedCapacity, actualProject.YearThreeProjectedCapacity),
-					() => Assert.Equal(expectedProject.YearThreeProjectedPupilNumbers, actualProject.YearThreeProjectedPupilNumbers),
-					() => Assert.Equal(expectedProject.KeyStage2PerformanceAdditionalInformation, actualProject.KeyStage2PerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.KeyStage4PerformanceAdditionalInformation, actualProject.KeyStage4PerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.KeyStage5PerformanceAdditionalInformation, actualProject.KeyStage5PerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.PreviousHeadTeacherBoardDateQuestion, actualProject.PreviousHeadTeacherBoardDateQuestion),
-					() => Assert.Equal(expectedProject.ConversionSupportGrantAmount, actualProject.ConversionSupportGrantAmount),
-					() => Assert.Equal(expectedProject.ConversionSupportGrantChangeReason, actualProject.ConversionSupportGrantChangeReason),
-					() => Assert.Equal(expectedProject.ProjectStatus, actualProject.ProjectStatus)
-				);
-	}
+		Assert.Equivalent(projectDetails, updatedProject.MapToDomain().Details);
+	}	
 }
--- Dfe.Academies.Academisation.Domain.UnitTest/ProjectAggregate/ProjectUpdateTests.cs
+++ Dfe.Academies.Academisation.Domain.UnitTest/ProjectAggregate/ProjectUpdateTests.cs
@@ -23,8 +23,10 @@ public class ProjectUpdateTests
 		var result = sut.Update(updatedProject);
 
 		// Assert
-		Assert.IsType<CommandSuccessResult>(result);
-		AssertProjectModelIsEqual(updatedProject, sut);
+		Assert.Multiple(
+			() => Assert.IsType<CommandSuccessResult>(result),
+			() => Assert.Equivalent(updatedProject, sut.Details)
+		);
 	}
 
 	[Fact]
@@ -40,60 +42,11 @@ public class ProjectUpdateTests
 
 		// Assert
 		var validationErrors = Assert.IsType<CommandValidationErrorResult>(result).ValidationErrors;
-		Assert.Equal("Urn", validationErrors.First().PropertyName);
-		Assert.Equal("Urn in update model must match existing record", validationErrors.First().ErrorMessage);
-		AssertProjectModelIsEqual(existingProject, sut);
-	}
 
-	private static void AssertProjectModelIsEqual(ProjectDetails expectedProject, Project sut)
-	{
 		Assert.Multiple(
-					() => Assert.Equal(expectedProject.HeadTeacherBoardDate, sut.Details.HeadTeacherBoardDate),
-					() => Assert.Equal(expectedProject.Author, sut.Details.Author),
-					() => Assert.Equal(expectedProject.ClearedBy, sut.Details.ClearedBy),
-					() => Assert.Equal(expectedProject.ProposedAcademyOpeningDate, sut.Details.ProposedAcademyOpeningDate),
-					() => Assert.Equal(expectedProject.PublishedAdmissionNumber, sut.Details.PublishedAdmissionNumber),
-					() => Assert.Equal(expectedProject.ViabilityIssues, sut.Details.ViabilityIssues),
-					() => Assert.Equal(expectedProject.FinancialDeficit, sut.Details.FinancialDeficit),
-					() => Assert.Equal(expectedProject.RationaleForProject, sut.Details.RationaleForProject),
-					() => Assert.Equal(expectedProject.RisksAndIssues, sut.Details.RisksAndIssues),
-					() => Assert.Equal(expectedProject.RevenueCarryForwardAtEndMarchCurrentYear, sut.Details.RevenueCarryForwardAtEndMarchCurrentYear),
-					() => Assert.Equal(expectedProject.ProjectedRevenueBalanceAtEndMarchNextYear, sut.Details.ProjectedRevenueBalanceAtEndMarchNextYear),
-					() => Assert.Equal(expectedProject.RationaleSectionComplete, sut.Details.RationaleSectionComplete),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateSentDate, sut.Details.LocalAuthorityInformationTemplateSentDate),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateReturnedDate, sut.Details.LocalAuthorityInformationTemplateReturnedDate),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateComments, sut.Details.LocalAuthorityInformationTemplateComments),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateLink, sut.Details.LocalAuthorityInformationTemplateLink),
-					() => Assert.Equal(expectedProject.LocalAuthorityInformationTemplateSectionComplete, sut.Details.LocalAuthorityInformationTemplateSectionComplete),
-					() => Assert.Equal(expectedProject.RecommendationForProject, sut.Details.RecommendationForProject),
-					() => Assert.Equal(expectedProject.AcademyOrderRequired, sut.Details.AcademyOrderRequired),
-					() => Assert.Equal(expectedProject.SchoolAndTrustInformationSectionComplete, sut.Details.SchoolAndTrustInformationSectionComplete),
-					() => Assert.Equal(expectedProject.DistanceFromSchoolToTrustHeadquarters, sut.Details.DistanceFromSchoolToTrustHeadquarters),
-					() => Assert.Equal(expectedProject.DistanceFromSchoolToTrustHeadquarters, sut.Details.DistanceFromSchoolToTrustHeadquarters),
-					() => Assert.Equal(expectedProject.DistanceFromSchoolToTrustHeadquartersAdditionalInformation, sut.Details.DistanceFromSchoolToTrustHeadquartersAdditionalInformation),
-					() => Assert.Equal(expectedProject.MemberOfParliamentName, sut.Details.MemberOfParliamentName),
-					() => Assert.Equal(expectedProject.MemberOfParliamentParty, sut.Details.MemberOfParliamentParty),
-					() => Assert.Equal(expectedProject.GeneralInformationSectionComplete, sut.Details.GeneralInformationSectionComplete),
-					() => Assert.Equal(expectedProject.RisksAndIssuesSectionComplete, sut.Details.RisksAndIssuesSectionComplete),
-					() => Assert.Equal(expectedProject.SchoolPerformanceAdditionalInformation, sut.Details.SchoolPerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.CapitalCarryForwardAtEndMarchCurrentYear, sut.Details.CapitalCarryForwardAtEndMarchCurrentYear),
-					() => Assert.Equal(expectedProject.CapitalCarryForwardAtEndMarchNextYear, sut.Details.CapitalCarryForwardAtEndMarchNextYear),
-					() => Assert.Equal(expectedProject.SchoolBudgetInformationAdditionalInformation, sut.Details.SchoolBudgetInformationAdditionalInformation),
-					() => Assert.Equal(expectedProject.SchoolAndTrustInformationSectionComplete, sut.Details.SchoolAndTrustInformationSectionComplete),
-					() => Assert.Equal(expectedProject.SchoolPupilForecastsAdditionalInformation, sut.Details.SchoolPupilForecastsAdditionalInformation),
-					() => Assert.Equal(expectedProject.YearOneProjectedCapacity, sut.Details.YearOneProjectedCapacity),
-					() => Assert.Equal(expectedProject.YearOneProjectedPupilNumbers, sut.Details.YearOneProjectedPupilNumbers),
-					() => Assert.Equal(expectedProject.YearTwoProjectedCapacity, sut.Details.YearTwoProjectedCapacity),
-					() => Assert.Equal(expectedProject.YearTwoProjectedPupilNumbers, sut.Details.YearTwoProjectedPupilNumbers),
-					() => Assert.Equal(expectedProject.YearThreeProjectedCapacity, sut.Details.YearThreeProjectedCapacity),
-					() => Assert.Equal(expectedProject.YearThreeProjectedPupilNumbers, sut.Details.YearThreeProjectedPupilNumbers),
-					() => Assert.Equal(expectedProject.KeyStage2PerformanceAdditionalInformation, sut.Details.KeyStage2PerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.KeyStage4PerformanceAdditionalInformation, sut.Details.KeyStage4PerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.KeyStage5PerformanceAdditionalInformation, sut.Details.KeyStage5PerformanceAdditionalInformation),
-					() => Assert.Equal(expectedProject.PreviousHeadTeacherBoardDateQuestion, sut.Details.PreviousHeadTeacherBoardDateQuestion),
-					() => Assert.Equal(expectedProject.ConversionSupportGrantAmount, sut.Details.ConversionSupportGrantAmount),
-					() => Assert.Equal(expectedProject.ConversionSupportGrantChangeReason, sut.Details.ConversionSupportGrantChangeReason),
-					() => Assert.Equal(expectedProject.ProjectStatus, sut.Details.ProjectStatus)
-				);
+			() => Assert.Equal("Urn", validationErrors.First().PropertyName),
+			() => Assert.Equal("Urn in update model must match existing record", validationErrors.First().ErrorMessage),
+			() => Assert.Equivalent(existingProject, sut.Details)
+		);
 	}
 }
