faf17a0d733a27bc75f8d03a376c66ba04450c21
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-09-15 17:33:03 +0100

    AB#106130 list projects - add search projects query with test

---
+++ Dfe.Academies.Academisation.Data.UnitTest/ProjectAggregate/ProjectListGetDataQueryTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using Dfe.Academies.Academisation.Data.ProjectAggregate;
+using Dfe.Academies.Academisation.Data.UnitTest.Contexts;
+using Dfe.Academies.Academisation.Domain.Core.ProjectAggregate;
+using Dfe.Academies.Academisation.Domain.ProjectAggregate;
+using Xunit;
+
+namespace Dfe.Academies.Academisation.Data.UnitTest.ProjectAggregate;
+
+public class ProjectsListGetDataQueryTests
+{
+	private readonly Fixture _fixture = new();
+
+	private readonly ProjectsListGetDataQuery _subject;
+	private readonly AcademisationContext _context;
+
+	public ProjectsListGetDataQueryTests()
+	{
+		_context = new TestProjectContext().CreateContext();
+		_subject = new ProjectsListGetDataQuery(_context);
+	}
+
+	[Fact]
+	public async Task ProjectsExists_SearchProjectsByState__ReturnsProjects()
+	{
+		// arrange
+		var (projectDetails1, projectState1) = CreateTestProject();
+		var (_, projectState2) = CreateTestProject();
+		var (_, projectState3) = CreateTestProject();
+
+		_context.Projects.Add(projectState1);
+		_context.Projects.Add(projectState2);
+		_context.Projects.Add(projectState3);
+
+		await _context.SaveChangesAsync();
+
+		// act
+		var searchStatus = new List<string> { projectState1!.ProjectStatus.ToLower() };
+		var result = await _subject.SearchProjects(searchStatus, 1, 1, null);
+
+		// assert
+		var firstProject = result.ToList().FirstOrDefault();
+		Assert.Multiple(
+			() => Assert.NotNull(firstProject),
+			() => Assert.Equal(firstProject!.Details, projectDetails1),
+			() => Assert.Equal(projectState1.Id, firstProject!.Id)
+		);
+	}
+
+	private Tuple<ProjectDetails, ProjectState> CreateTestProject()
+	{
+		var projectDetails = _fixture.Create<ProjectDetails>();
+		var newProject = new Project(0, projectDetails);
+		var mappedProject = ProjectState.MapFromDomain(newProject);
+
+		return Tuple.Create(projectDetails, mappedProject);
+	}
+}
---
+++ Dfe.Academies.Academisation.Data/ProjectAggregate/ProjectListGetDataQuery.cs
@@ -0,0 +1,39 @@
+﻿using Dfe.Academies.Academisation.IData.ProjectAggregate;
+using Dfe.Academies.Academisation.IDomain.ProjectAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dfe.Academies.Academisation.Data.ProjectAggregate
+{
+	public class ProjectsListGetDataQuery : IProjectsListGetDataQuery
+	{
+		private readonly AcademisationContext _context;
+
+		public ProjectsListGetDataQuery(AcademisationContext context)
+		{
+			_context = context;
+		}
+		
+		public async Task<IEnumerable<IProject>> SearchProjects(List<string>? states, int page, int count, int? urn)
+		{
+			IQueryable<ProjectState> queryable = _context.Projects
+				.OrderByDescending(acp => acp.ApplicationReceivedDate);
+			
+			if (states != null && states!.Any())
+			{
+				// var lowerStatuses = states.Select(status => status.ToLower());
+				queryable = queryable.Where(p => states.Contains(p.ProjectStatus!.ToLower()));
+			}
+
+			if (urn.HasValue)
+			{
+				queryable = queryable.Where(acp => acp.Urn == urn);
+			}
+
+			var projects =  await queryable
+				.Skip((page - 1) * count)
+				.Take(count).ToListAsync();
+			
+			return projects.Select(p => p.MapToDomain());
+		}
+	}
+}
