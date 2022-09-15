7585beea654456f3c8c115893f00e9fd44656446
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-09-15 17:34:20 +0100

    AB#106130 list projects - adding empty queries

---
+++ Dfe.Academies.Academisation.IData/ProjectAggregate/IProjectsListGetDataQuery.cs
@@ -0,0 +1,10 @@
+﻿using Dfe.Academies.Academisation.IDomain.ProjectAggregate;
+
+namespace Dfe.Academies.Academisation.IData.ProjectAggregate
+{
+	public interface IProjectsListGetDataQuery
+	{
+		Task<IEnumerable<IProject>> SearchProjects(
+			List<string> states, int page, int count, int? urn);
+	}
+}
---
+++ Dfe.Academies.Academisation.Service/Queries/LegacyProjectsListGetQuery.cs
@@ -0,0 +1,15 @@
+﻿using Dfe.Academies.Academisation.IService.Query;
+using Dfe.Academies.Academisation.IService.ServiceModels.Legacy.ProjectAggregate;
+
+namespace Dfe.Academies.Academisation.Service.Queries
+{
+	public class LegacyProjectsListGetQuery : ILegacyProjectsListGetQuery 
+	{
+		public async Task<LegacyProjectServiceModel> GetProjects(
+			string states, int page, int count, int? urn)
+		{
+			await Task.CompletedTask;
+			throw new NotImplementedException("");
+		}
+	}
+}
