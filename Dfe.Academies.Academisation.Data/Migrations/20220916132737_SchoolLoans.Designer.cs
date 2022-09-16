810b7a7c300f96890f6afc16cf88518015229f11
mark robinson <mark.robinson@education.gov.uk>
2022-09-16 14:32:08 +0100

    AB#105465 - API - School Loans - EF migration for new table

--- Dfe.Academies.Academisation.Data/Migrations/20220916132737_SchoolLoans.Designer.cs
+++ Dfe.Academies.Academisation.Data/Migrations/20220916132737_SchoolLoans.Designer.cs
@@ -4,6 +4,7 @@ using Dfe.Academies.Academisation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dfe.Academies.Academisation.Data.Migrations
 {
     [DbContext(typeof(AcademisationContext))]
-    partial class AcademisationContextModelSnapshot : ModelSnapshot
+    [Migration("20220916132737_SchoolLoans")]
+    partial class SchoolLoans
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -530,6 +532,43 @@ namespace Dfe.Academies.Academisation.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("ConversionAdvisoryBoardDecision", "academisation");
+                });
+
+            modelBuilder.Entity("Dfe.Academies.Academisation.Data.ConversionLegalRequirement.LegalRequirementState", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<DateTime>("CreatedOn")
+                        .HasColumnType("datetime2");
+
+                    b.Property<int?>("HadConsultation")
+                        .HasColumnType("int");
+
+                    b.Property<int?>("HasDioceseConsented")
+                        .HasColumnType("int");
+
+                    b.Property<int?>("HasFoundationConsented")
+                        .HasColumnType("int");
+
+                    b.Property<int?>("HaveProvidedResolution")
+                        .HasColumnType("int");
+
+                    b.Property<bool>("IsSectionComplete")
+                        .HasColumnType("bit");
+
+                    b.Property<DateTime>("LastModifiedOn")
+                        .HasColumnType("datetime2");
+
+                    b.Property<int>("ProjectId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("LegalRequirement", "academisation");
                 });
 
             modelBuilder.Entity("Dfe.Academies.Academisation.Data.ProjectAggregate.ProjectState", b =>
---
+++ Dfe.Academies.Academisation.Data/Migrations/20220916132737_SchoolLoans.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Academies.Academisation.Data.Migrations
+{
+    public partial class SchoolLoans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationSchoolLoan",
+                schema: "academisation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Purpose = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Provider = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InterestRate = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Schedule = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApplicationSchoolId = table.Column<int>(type: "int", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationSchoolLoan", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationSchoolLoan_ApplicationSchool_ApplicationSchoolId",
+                        column: x => x.ApplicationSchoolId,
+                        principalSchema: "academisation",
+                        principalTable: "ApplicationSchool",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationSchoolLoan_ApplicationSchoolId",
+                schema: "academisation",
+                table: "ApplicationSchoolLoan",
+                column: "ApplicationSchoolId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationSchoolLoan",
+                schema: "academisation");
+        }
+    }
+}
