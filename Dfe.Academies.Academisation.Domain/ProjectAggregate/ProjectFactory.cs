0faa3d558a6ae0328bca6aa02abe73b67005e256
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-11-28 17:38:25 +0000

    AB#112498 map project - removing local authority from project

--- Dfe.Academies.Academisation.Domain/ProjectAggregate/ProjectFactory.cs
+++ Dfe.Academies.Academisation.Domain/ProjectAggregate/ProjectFactory.cs
@@ -5,7 +5,7 @@ using Dfe.Academies.Academisation.IDomain.ProjectAggregate;
 namespace Dfe.Academies.Academisation.Domain.ProjectAggregate;
 
 public class ProjectFactory : IProjectFactory
-{		
+{
 	public CreateResult<IProject> Create(IApplication application)
 	{
 		return Project.Create(application);
