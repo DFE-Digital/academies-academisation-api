d88059214793b9dacd1a5d164d43075dc2c931b6
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-12-02 20:06:06 +0000

    add more unit tests

---
+++ Dfe.Academies.Academisation.Service.UnitTest/Commands/Legacy/Project/EnrichProjectCommandTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using Dfe.Academies.Academisation.IData.Establishment;
+using Dfe.Academies.Academisation.IData.ProjectAggregate;
+using Dfe.Academies.Academisation.IDomain.ProjectAggregate;
+using Dfe.Academies.Academisation.Service.Commands.Legacy.Project;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Dfe.Academies.Academisation.Service.UnitTest.Commands.Legacy.Project
+{
+	public class EnrichProjectCommandTests
+	{
+		private readonly EnrichProjectCommand _sut;
+		private readonly Mock<IIncompleteProjectsGetDataQuery> _incompleteProjectGetDataQuery;
+		private readonly Mock<IEstablishmentGetDataQuery> _establishmentGetDataQuery;
+		private readonly Mock<IProjectUpdateDataCommand> _updateCommand;
+		private readonly Mock<ILogger<EnrichProjectCommand>> _logger;
+
+		private readonly Fixture _fixture = new();
+
+		public EnrichProjectCommandTests()
+		{
+			_incompleteProjectGetDataQuery = new Mock<IIncompleteProjectsGetDataQuery>();
+			_establishmentGetDataQuery = new Mock<IEstablishmentGetDataQuery>();
+			_updateCommand = new Mock<IProjectUpdateDataCommand>();
+			_logger = new Mock<ILogger<EnrichProjectCommand>>();
+
+			_sut = new EnrichProjectCommand(
+				_logger.Object,
+				_incompleteProjectGetDataQuery.Object,
+				_establishmentGetDataQuery.Object,
+				_updateCommand.Object);
+		}
+
+		[Fact]
+		public async Task NoIncompleteProjects__LogAndReturn()
+		{
+			await _sut.Execute();
+
+			Assert.Multiple(
+				() => VerifyLogging(_logger, "No projects requiring enrichment found."),
+				() => _establishmentGetDataQuery.VerifyNoOtherCalls(),
+				() => _updateCommand.VerifyNoOtherCalls());
+
+		}
+
+		[Fact]
+		public async Task IncompleteProjects__UnknownSchool__LogAndContinue()
+		{
+			var projects = _fixture.CreateMany<Domain.ProjectAggregate.Project>();
+			_incompleteProjectGetDataQuery.Setup(m => m.GetIncompleteProjects())
+				.ReturnsAsync(projects);
+
+			_establishmentGetDataQuery.SetupSequence(m => m.GetEstablishment(It.IsAny<int>()))
+				.ReturnsAsync((Establishment?)null)
+				.ReturnsAsync(_fixture.Create<Establishment>())
+				.ReturnsAsync(_fixture.Create<Establishment>());
+
+			await _sut.Execute();
+
+			Assert.Multiple(
+				() => VerifyLogging(_logger, $"No schools found for project - {projects.First().Id}, urn - {projects.First().Details.Urn}",
+					LogLevel.Warning),
+				() => _updateCommand.Verify(m => m.Execute(It.IsAny<IProject>()), Times.Exactly(2)));
+
+		}
+
+		[Fact]
+		public async Task IncompleteProjects__KnownSchool__UpdateDb()
+		{
+			var project = _fixture.Create<Domain.ProjectAggregate.Project>();
+			_incompleteProjectGetDataQuery.Setup(m => m.GetIncompleteProjects())
+				.ReturnsAsync(new List<IProject> { project });
+
+			_establishmentGetDataQuery.Setup(m => m.GetEstablishment(It.IsAny<int>()))
+				.ReturnsAsync(_fixture.Create<Establishment>());
+
+			await _sut.Execute();
+
+			Assert.Multiple(
+				() => _updateCommand.Verify(m => m.Execute(project), Times.Once));
+
+		}
+
+		public static Mock<ILogger<T>> VerifyLogging<T>(Mock<ILogger<T>> logger, string expectedMessage, LogLevel expectedLogLevel = LogLevel.Information, Times? times = null)
+		{
+			times ??= Times.Once();
+
+			Func<object, Type, bool> state = (v, t) => v!.ToString().CompareTo(expectedMessage) == 0;
+
+			logger.Verify(
+				x => x.Log(
+					It.Is<LogLevel>(l => l == expectedLogLevel),
+					It.IsAny<EventId>(),
+					It.Is<It.IsAnyType>((v, t) => state(v, t)),
+					It.IsAny<Exception>(),
+					It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), (Times)times);
+
+			return logger;
+		}
+	}
+}
