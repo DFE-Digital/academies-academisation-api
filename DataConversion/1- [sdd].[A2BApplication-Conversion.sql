a2a348fb3737c462212cff31b7b94610ea6a5550
Mark ROBINSON <Mark.ROBINSON@EDUCATION.GOV.UK>
2022-12-06 15:21:15 +0000

    added & modified scripts

--- DataConversion/1- [sdd].[A2BApplication-Conversion.sql
+++ DataConversion/1- [sdd].[A2BApplication-Conversion.sql
@@ -1,10 +1,12 @@
 BEGIN TRY
-BEGIN TRANSACTION PortDynamicsData
+BEGIN TRANSACTION PortDynamicsApplicationData
 
-	/*** populate [academisation].[ConversionApplication] ***/
-	-- MR:- below are nullable - backfill afterwards?
---,<FormTrustId, int,>
---,<JoinTrustId, int,>
+	/*** TODO:- set [academisation].[ConversionApplication].[Id] = 10000 ***/
+
+	/*** STEP 1 - populate [academisation].[ConversionApplication] ***/
+	-- TODO MR:- below are nullable - backfill as 4th / 5th step
+	--,<FormTrustId, int,>
+	--,<JoinTrustId, int,>
 
 	INSERT INTO [academisation].[ConversionApplication]
            ([ApplicationType]
@@ -23,15 +25,107 @@ BEGIN TRANSACTION PortDynamicsData
 --           ,<ApplicationStatus, nvarchar(max),>
 --           ,<FormTrustId, int,>
 --           ,<JoinTrustId, int,>
---           ,<ApplicationSubmittedDate, datetime2(7),>
---           ,<ApplicationReference, nvarchar(max),>
---           ,<DynamicsApplicationId, uniqueidentifier,>)
+--           ,<ApplicationSubmittedDate, datetime2(7),>)
 
-	SELECT
+	SELECT	[ApplicationType],
+			GETDATE() as 'CreatedOn',
+			GETDATE() as 'LastModifiedOn',
+		-- TODO:- the rest !!!!
+			[Name] as 'ApplicationReference',
+			[DynamicsApplicationId]
 	FROM [sdd].[A2BApplication]
 
-	COMMIT TRAN PortDynamicsData
-	--ROLLBACK TRAN PortDynamicsData
+	/*** STEP 2 - populate [academisation].[ApplicationJoinTrust] ***/
+	INSERT INTO [academisation].[ApplicationJoinTrust]
+			   ([UKPRN]
+			   ,[TrustName]
+			   ,[CreatedOn]
+			   ,[LastModifiedOn]
+			   ,[ChangesToTrust]
+			   ,[ChangesToTrustExplained]
+			   ,[ChangesToLaGovernance]
+			   ,[ChangesToLaGovernanceExplained])
+	--     VALUES
+	--           (<UKPRN, int,>
+	--           ,<TrustName, nvarchar(max),>
+	--           ,<CreatedOn, datetime2(7),>
+	--           ,<LastModifiedOn, datetime2(7),>
+	--           ,<ChangesToTrust, int,>
+	--           ,<ChangesToTrustExplained, nvarchar(max),>
+	--           ,<ChangesToLaGovernance, bit,>
+	--           ,<ChangesToLaGovernanceExplained, nvarchar(max),>)
+
+	SELECT 	GETDATE() as 'CreatedOn',
+			GETDATE() as 'LastModifiedOn',
+		-- TODO:- the rest !!!!
+			[DynamicsApplicationId]
+	FROM [sdd].[A2BApplication]
+	WHERE ApplicationType = 'JAM';
+	
+	/*** STEP 3 - populate [academisation].[ApplicationFormTrust] ***/
+	INSERT INTO [academisation].[ApplicationFormTrust]
+           ([TrustApproverName]
+           ,[CreatedOn]
+           ,[LastModifiedOn]
+           ,[FormTrustGrowthPlansYesNo]
+           ,[FormTrustImprovementApprovedSponsor]
+           ,[FormTrustImprovementStrategy]
+           ,[FormTrustImprovementSupport]
+           ,[FormTrustOpeningDate]
+           ,[FormTrustPlanForGrowth]
+           ,[FormTrustPlansForNoGrowth]
+           ,[FormTrustProposedNameOfTrust]
+           ,[FormTrustReasonApprovaltoConvertasSAT]
+           ,[FormTrustReasonApprovedPerson]
+           ,[FormTrustReasonForming]
+           ,[FormTrustReasonFreedom]
+           ,[FormTrustReasonGeoAreas]
+           ,[FormTrustReasonImproveTeaching]
+           ,[FormTrustReasonVision]
+           ,[TrustApproverEmail])
+     --VALUES
+     --      (<TrustApproverName, nvarchar(max),>
+     --      ,<CreatedOn, datetime2(7),>
+     --      ,<LastModifiedOn, datetime2(7),>
+     --      ,<FormTrustGrowthPlansYesNo, bit,>
+     --      ,<FormTrustImprovementApprovedSponsor, nvarchar(max),>
+     --      ,<FormTrustImprovementStrategy, nvarchar(max),>
+     --      ,<FormTrustImprovementSupport, nvarchar(max),>
+     --      ,<FormTrustOpeningDate, datetime2(7),>
+     --      ,<FormTrustPlanForGrowth, nvarchar(max),>
+     --      ,<FormTrustPlansForNoGrowth, nvarchar(max),>
+     --      ,<FormTrustProposedNameOfTrust, nvarchar(max),>
+     --      ,<FormTrustReasonApprovaltoConvertasSAT, bit,>
+     --      ,<FormTrustReasonApprovedPerson, nvarchar(max),>
+     --      ,<FormTrustReasonForming, nvarchar(max),>
+     --      ,<FormTrustReasonFreedom, nvarchar(max),>
+     --      ,<FormTrustReasonGeoAreas, nvarchar(max),>
+     --      ,<FormTrustReasonImproveTeaching, nvarchar(max),>
+     --      ,<FormTrustReasonVision, nvarchar(max),>
+     --      ,<TrustApproverEmail, nvarchar(max),>)
+	SELECT 	GETDATE() as 'CreatedOn',
+			GETDATE() as 'LastModifiedOn',
+		-- TODO:- the rest !!!!
+			[DynamicsApplicationId]
+	FROM [sdd].[A2BApplication]
+	WHERE ApplicationType = 'FAM';
+
+	/*** STEP 4 - backfill <FormTrustId, int,> from ***/
+	-- TODO MR:- need to add [DynamicsApplicationId] to [academisation].[ApplicationFormTrust]
+	UPDATE CA
+	SET CA.FormTrustId = AFT.Id
+	FROM [academisation].[ConversionApplication] As CA
+	INNER JOIN [academisation].[ApplicationFormTrust] as AFT ON AFT.[DynamicsApplicationId] = CA.[DynamicsApplicationId]
+
+	/*** STEP 5 - backfill <JoinTrustId, int,> ***/
+	-- TODO MR:- need to add [DynamicsApplicationId] to [academisation].[ApplicationJoinTrust]
+	UPDATE CA
+	SET CA.FormTrustId = AJT.Id
+	FROM [academisation].[ConversionApplication] As CA
+	INNER JOIN [academisation].[ApplicationJoinTrust] as AJT ON AJT.[DynamicsApplicationId] = CA.[DynamicsApplicationId]
+
+	COMMIT TRAN PortDynamicsApplicationData
+	--ROLLBACK TRAN PortDynamicsApplicationData
 
 END TRY
 BEGIN CATCH
---
+++ DataConversion/3- [sdd].[A2BSchoolLease-Conversion.sql
@@ -0,0 +1,73 @@
+BEGIN TRY
+BEGIN TRANSACTION PortDynamicsSchoolLeaseData
+
+	/*** STEP 1 - populate [academisation].[ApplicationSchoolLease] ***/
+
+	-- MR:- need to grab DB generated [ApplicationSchoolId]
+	-- by joining onto [academisation].[ApplicationSchool]
+	INSERT INTO [academisation].[ApplicationSchoolLease]
+			   ([LeaseTerm]
+			   ,[RepaymentAmount]
+			   ,[InterestRate]
+			   ,[PaymentsToDate]
+			   ,[Purpose]
+			   ,[ValueOfAssets]
+			   ,[ResponsibleForAssets]
+			   ,[ApplicationSchoolId]
+			   ,[CreatedOn]
+			   ,[LastModifiedOn]
+			   ,[DynamicsSchoolLeaseId])
+     --VALUES
+     --      (<LeaseTerm, nvarchar(max),>
+     --      ,<RepaymentAmount, decimal(18,2),>
+     --      ,<InterestRate, decimal(18,2),>
+     --      ,<PaymentsToDate, decimal(18,2),>
+     --      ,<Purpose, nvarchar(max),>
+     --      ,<ValueOfAssets, nvarchar(max),>
+     --      ,<ResponsibleForAssets, nvarchar(max),>
+     --      ,<ApplicationSchoolId, int,>
+     --      ,<CreatedOn, datetime2(7),>
+     --      ,<LastModifiedOn, datetime2(7),>
+     --      ,<DynamicsSchoolLeaseId, uniqueidentifier,>)
+
+	SELECT ASL.SchoolLeaseTerm,
+			-- TODO:- the rest !!!!
+			SCH.Id as 'ApplicationSchoolId',
+			GETDATE() as 'CreatedOn',
+			GETDATE() as 'LastModifiedOn',
+			ASL.DynamicsSchoolLeaseId
+			--ASS.[DynamicsApplyingSchoolId]
+	FROM [sdd].[A2BApplicationApplyingSchool] As ASS
+	INNER JOIN [academisation].[ApplicationSchool] as SCH on SCH.DynamicsApplyingSchoolId = ASS.DynamicsApplyingSchoolId
+	INNER JOIN [sdd].[A2BSchoolLease] as ASL ON ASL.ApplyingSchoolId = ASS.ApplyingSchoolId
+	   
+	/* STEP 2 - backfill [academisation].[ApplicationSchool].HasLeases */
+	-- MR:- below are nullable - backfill afterwards - as part of leases && loans conversion
+	--,[HasLeases]
+	UPDATE SCH
+	SET SCH.HasLeases = 1
+	FROM [academisation].[ApplicationSchool] as SCH
+	INNER JOIN [academisation].[ApplicationSchoolLease] as ASL ON ASL.ApplicationSchoolId = SCH.Id
+
+	--COMMIT TRAN PortDynamicsSchoolLeaseData
+	--ROLLBACK TRAN PortDynamicsSchoolLeaseData
+
+END TRY
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_STATE() AS ErrorState,
+    ERROR_SEVERITY() AS ErrorSeverity,
+    ERROR_PROCEDURE() AS ErrorProcedure,
+    ERROR_LINE() AS ErrorLine,
+    ERROR_MESSAGE() AS ErrorMessage;
+
+-- Transaction uncommittable
+    IF (XACT_STATE()) = -1
+      ROLLBACK TRANSACTION
+ 
+-- Transaction committable
+    IF (XACT_STATE()) = 1
+      COMMIT TRANSACTION
+END CATCH;
+GO
