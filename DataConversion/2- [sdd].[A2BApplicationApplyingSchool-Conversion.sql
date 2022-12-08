9aacb541b118d3bea106767a737f5ac953c9a151
Mark ROBINSON <Mark.ROBINSON@EDUCATION.GOV.UK>
2022-12-08 14:06:58 +0000

    fixed column number mis-match

--- DataConversion/2- [sdd].[A2BApplicationApplyingSchool-Conversion.sql
+++ DataConversion/2- [sdd].[A2BApplicationApplyingSchool-Conversion.sql
@@ -51,44 +51,35 @@ BEGIN TRANSACTION PortDynamicsSchoolData
 			   ,[ConversionChangeNamePlanned]
 			   ,[ConfirmPaySupportGrantToSchool]
 			   ,[SupportGrantFundsPaidTo]
-			   ,[DioceseFolderIdentifier]
 			   ,[DioceseName]
-			   ,[FoundationConsentFolderIdentifier]
 			   ,[FoundationTrustOrBodyName]
 			   ,[FurtherInformation]
 			   ,[LocalAuthorityClosurePlanDetails]
 			   ,[LocalAuthorityReoganisationDetails]
 			   ,[OfstedInspectionDetails]
-			   ,[ResolutionConsentFolderIdentifier]
 			   ,[SafeguardingDetails]
 			   ,[TrustBenefitDetails]
 			   ,[CurrentFinancialYearCapitalCarryForward]
 			   ,[CurrentFinancialYearCapitalCarryForwardExplained]
-			   ,[CurrentFinancialYearCapitalCarryForwardFileLink]
 			   ,[CurrentFinancialYearCapitalCarryForwardStatus]
 			   ,[CurrentFinancialYearEndDate]
 			   ,[CurrentFinancialYearRevenue]
 			   ,[CurrentFinancialYearRevenueStatus]
 			   ,[CurrentFinancialYearRevenueStatusExplained]
-			   ,[CurrentFinancialYearRevenueStatusFileLink]
 			   ,[NextFinancialYearCapitalCarryForward]
 			   ,[NextFinancialYearCapitalCarryForwardExplained]
-			   ,[NextFinancialYearCapitalCarryForwardFileLink]
 			   ,[NextFinancialYearCapitalCarryForwardStatus]
 			   ,[NextFinancialYearEndDate]
 			   ,[NextFinancialYearRevenue]
 			   ,[NextFinancialYearRevenueStatus]
 			   ,[NextFinancialYearRevenueStatusExplained]
-			   ,[NextFinancialYearRevenueStatusFileLink]
 			   ,[PreviousFinancialYearCapitalCarryForward]
 			   ,[PreviousFinancialYearCapitalCarryForwardExplained]
-			   ,[PreviousFinancialYearCapitalCarryForwardFileLink]
 			   ,[PreviousFinancialYearCapitalCarryForwardStatus]
 			   ,[PreviousFinancialYearEndDate]
 			   ,[PreviousFinancialYearRevenue]
 			   ,[PreviousFinancialYearRevenueStatus]
 			   ,[PreviousFinancialYearRevenueStatusExplained]
-			   ,[PreviousFinancialYearRevenueStatusFileLink]
 			   ,[SchoolHasConsultedStakeholders]
 			   ,[SchoolPlanToConsultStakeholders]
 			   ,[FinanceOngoingInvestigations]
@@ -102,11 +93,10 @@ BEGIN TRANSACTION PortDynamicsSchoolData
 			   ,[MainFeederSchools]
 			   ,[PartOfFederation]
 			   ,[ProtectedCharacteristics]
-			   ,[HasLeases]
-			   ,[HasLoans]
 			   ,[DynamicsApplyingSchoolId])
-	SELECT ASS.[Urn],
-			APP.Id,
+	SELECT 
+			ASS.[Urn],
+			APP.Id as [ConversionApplicationId],
 			GETDATE() as 'CreatedOn',
 			GETDATE() as 'LastModifiedOn',
 			ASS.[ProjectedPupilNumbersYear1],
@@ -125,13 +115,12 @@ BEGIN TRANSACTION PortDynamicsSchoolData
 			ASS.[SchoolConversionContactHeadName],
 			ASS.[SchoolConversionContactHeadTel],
 			ASS.[SchoolConversionContactRole],
-			--[SchoolConversionTargetDateDifferent] -- not captured v1.5
 			ASS.[SchoolConversionTargetDateExplained],
 			ASS.[SchoolConversionReasonsForJoining], --JoinTrustReason  -- TODO:- check spreadsheet
 			ASS.[SchoolConversionMainContactOtherEmail],
 			ASS.[SchoolConversionMainContactOtherName],
 			ASS.[SchoolConversionMainContactOtherRole],
-			ASS.[Name],  -- TODO:- check spreadsheet
+			ASS.[Name] as [SchoolName],  -- TODO:- check spreadsheet
 			--**** land & buildings ****
 			ASS.[SchoolBuildLandSharedFacilities],
 			ASS.[SchoolBuildLandSharedFacilitiesExplained],
@@ -147,15 +136,14 @@ BEGIN TRANSACTION PortDynamicsSchoolData
 			ASS.[SchoolBuildLandWorksPlannedExplained],
 			-- ****
 			ASS.[SchoolConversionTargetDateDate],
-			ASS.[SchoolConversionTargetDateDifferent] as 'ConversionChangeNamePlanned',-- TODO:- check spreadsheet
+			ASS.[SchoolConversionTargetDateDifferent], -- bool
+			ASS.[SchoolConversionChangeName], -- bool
 			0 as 'ConfirmPaySupportGrantToSchool', -- TODO:- check spreadsheet
 			--ASS.[SchoolSupportGrantFundsPaidTo],
 			0 as 'SupportGrantFundsPaidTo', -- TODO:- check spreadsheet
 			--**** additional info ****
 			--ASS.[SchoolFaithSchool] - not in v1.5 schema ??
-			NULL as 'DioceseFolderIdentifier',
-			ASS.[SchoolFaithSchoolDioceseName],
-			NULL as 'FoundationConsentFolderIdentifier',
+			ASS.[SchoolFaithSchoolDioceseName] AS [DioceseName],
 			ASS.[SchoolSupportedFoundationBodyName], -- TODO:- check spreadsheet
 			ASS.[SchoolFurtherInformation], -- TODO:- check spreadsheet
 			--ASS.[SchoolLaClosurePlans], - not in v1.5 schema ??
@@ -164,38 +152,31 @@ BEGIN TRANSACTION PortDynamicsSchoolData
 			ASS.[SchoolLaReorganizationExplain],
 			--ASS.[SchoolAdInspectedButReportNotPublished], - not in v1.5 schema ??
 			ASS.[SchoolAdInspectedReportNotPublishedExplain],
-			NULL AS 'ResolutionConsentFolderIdentifier',
 			--ASS.[SchoolAdSafeguarding], - not in v1.5 schema ??
 			ASS.[SchoolAdSafeguardingExplained] as 'SafeguardingDetails',
 			'' as 'TrustBenefitDetails', -- TODO:- check spreadsheet
 			-- **** CFY / NFY / PFY details ****
 			ASS.[SchoolCFYCapitalForward],
 			ASS.[SchoolCFYCapitalForwardStatusExplained],
-			NULL as 'CurrentFinancialYearCapitalCarryForwardFileLink',
 			ASS.[SchoolCFYCapitalIsDeficit], -- TODO:- check spreadsheet. bit -> int v1.5
 			ASS.[SchoolCFYEndDate],
 			ASS.[SchoolCFYRevenue],
 			ASS.[SchoolCFYRevenueIsDeficit], -- TODO:- check spreadsheet. bit -> int v1.5
 			ASS.[SchoolCFYRevenueStatusExplained],
-			NULL as 'CurrentFinancialYearRevenueStatusFileLink',
 			ASS.[SchoolNFYCapitalForward],
-			ASS.[SchoolCFYCapitalForwardStatusExplained],
-			NULL as 'NextFinancialYearCapitalCarryForwardFileLink',
-			ASS.[SchoolNFYRevenueIsDeficit],  -- TODO:- check spreadsheet. bit -> int v1.5
+			ASS.[SchoolNFYCapitalForwardStatusExplained],
+			ASS.[SchoolNFYCapitalIsDeficit],  -- TODO:- check spreadsheet. bit -> int v1.5
 			ASS.[SchoolNFYEndDate],
 			ASS.[SchoolNFYRevenue],
 			ASS.[SchoolNFYRevenueIsDeficit],  -- TODO:- check spreadsheet. bit -> int v1.5
 			ASS.[SchoolNFYRevenueStatusExplained],
-			NULL as 'NextFinancialYearRevenueStatusFileLink',
 			ASS.[SchoolPFYCapitalForward],
 			ASS.[SchoolPFYCapitalForwardStatusExplained],
-			NULL as 'PreviousFinancialYearCapitalCarryForwardFileLink',
 			ASS.[SchoolPFYCapitalIsDeficit], -- TODO:- check spreadsheet. bit -> int v1.5
 			ASS.[SchoolPFYEndDate],
 			ASS.[SchoolPFYRevenue],
 			ASS.[SchoolPFYRevenueIsDeficit],
 			ASS.[SchoolPFYRevenueStatusExplained],
-			NULL as 'PreviousFinancialYearRevenueStatusFileLink',
 			-- ****
 			ASS.[SchoolConsultationStakeholders],
 			ASS.[SchoolConsultationStakeholdersConsult],
@@ -205,18 +186,16 @@ BEGIN TRANSACTION PortDynamicsSchoolData
 			-- ****
 			ASS.[SchoolDeclarationBodyAgree],
 			ASS.[SchoolDeclarationTeacherChair],
-			ASS.[SchoolDeclarationSignedByEmail], -- TODO:- check spreadsheet.
+			ASS.[SchoolDeclarationSignedByEmail] as 'DeclarationSignedByName', -- TODO:- check spreadsheet.
 			-- ****
 			ASS.[SchoolConversionReasonsForJoining],
-			-- ****
-			--ASS.[SchoolSACREExemption], - not in v1.5 schema ??
+			-- **** more additional info
+			--ASS.[SchoolSACREExemption], BOOL - not in v1.5 schema ??
 			ASS.[SchoolSACREExemptionEndDate],
 			-- ****
 			ASS.[SchoolAdFeederSchools],
 			ASS.[SchoolPartOfFederation],
 			'' as 'ProtectedCharacteristics', -- TODO:- check spreadsheet
-			NULL as HasLeases, --populated in later script
-			NULL as HasLoans, --populated in later script
 			ASS.[DynamicsApplyingSchoolId]
 	FROM [sdd].[A2BApplicationApplyingSchool] as ASS
 	INNER JOIN [academisation].[ConversionApplication] As APP ON APP.DynamicsApplicationId = ASS.DynamicsApplicationId
