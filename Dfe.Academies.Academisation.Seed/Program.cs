0b0360ae9edd7b704fbbe8e516122b6e6f4ae16e
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-01-10 16:51:48 +0000

    Using batches to inprove performance

--- Dfe.Academies.Academisation.Seed/Program.cs
+++ Dfe.Academies.Academisation.Seed/Program.cs
@@ -11,22 +11,11 @@ var stopwatch = new Stopwatch();
 // Get user input on number of projects to create
 Console.WriteLine("Hello, please enter a number of projects you would like to seed.");
 int numberOfProjects = Convert.ToInt32(Console.ReadLine());
-
-// Set up progress bar
 Console.WriteLine("Generating new projects... ");
-using (var progress = new ProgressBar())
-{
-	stopwatch.Start();
-	// Begin creation of projects and project notes for the input number
-	for (int i = 1; i <= numberOfProjects; i++)
-	{
-		SeedProject.CreateProject(context);
-		// Add Application
-		// TODO: When the switch happens for application over to academisation
+stopwatch.Start();
 
-		// Report progress to update visual
-		progress.Report((double)i / numberOfProjects);
-	}
-	stopwatch.Stop();
-}
+// Begin creation of projects and project notes for the input number
+await SeedProject.CreateProject(context, numberOfProjects);
+stopwatch.Stop();
+
 Console.WriteLine($"Done in {stopwatch.Elapsed.Minutes} minutes and {stopwatch.Elapsed.Seconds} seconds");
--- Dfe.Academies.Academisation.Seed/SeedProject.cs
+++ Dfe.Academies.Academisation.Seed/SeedProject.cs
@@ -9,59 +9,109 @@ namespace Dfe.Academies.Academisation.Seed;
 
 public static class SeedProject
 {
-	public static void CreateProject(AcademisationContext? academisationContext)
+	//public static async Task CreateProject(AcademisationContext academisationContext, int numberOfProjects)
+	//{
+	//	var fixture = new Fixture();
+	//	for (int i = 1; i <= numberOfProjects; i++)
+	//	{
+	//		var newAcademyConversionProject = NewAcademyConversionProject();
+	//		var projectNote = fixture.Create<ProjectNoteState>();
+
+	//		using var dbContextTransaction = await academisationContext.Database.BeginTransactionAsync();
+	//		try
+	//		{
+	//			// Create and add project
+	//			var newProjectState = ProjectState.MapFromDomain(newAcademyConversionProject);
+	//			newProjectState.Id = default;
+	//			academisationContext.Projects.Add(newProjectState);
+	//			await academisationContext.SaveChangesAsync();
+
+	//			projectNote.ProjectId = newProjectState.Id;
+	//			projectNote.Id = default;
+	//			academisationContext.ProjectNotes.Add(projectNote);
+	//			await academisationContext.SaveChangesAsync();
+
+	//			await dbContextTransaction.CommitAsync();
+	//		}
+	//		catch (Exception ex)
+	//		{
+	//			await dbContextTransaction.RollbackAsync();
+	//			// better logging and error handling strategy
+	//			Console.WriteLine(ex.ToString());
+	//			throw;
+	//		}
+	//	}
+	//}
+
+
+	public static async Task CreateProject(AcademisationContext academisationContext, int numberOfProjects)
 	{
 		var fixture = new Fixture();
-		using var dbContextTransaction = academisationContext?.Database.BeginTransaction();
-		try
+		var newProjectStates = new List<ProjectState>();
+		var projectNotes = new List<ProjectNoteState>();
+		for (int i = 1; i <= numberOfProjects; i++)
 		{
-			// Create and add project
-			Project newAcademyConversionProject = NewAcademyConversionProject();
-			var newProjectState = ProjectState.MapFromDomain(newAcademyConversionProject);
-
-			// Clear Id as EF will throw an exception otherwise
-			newProjectState.Id = default;
-
-			// Save project
-			academisationContext?.Projects.Add(newProjectState);
-			academisationContext?.SaveChanges();
-
-			// Create and add project note
-			var projectNote = fixture.Create<ProjectNoteState>();
-			projectNote.ProjectId = newProjectState.Id;
-
-			// Clear Id as EF will throw an exception otherwise
-			projectNote.Id = default;
-			academisationContext?.ProjectNotes.Add(projectNote);
-			academisationContext?.SaveChanges();
-
-			dbContextTransaction?.Commit();
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine(ex.ToString());
-		}
-
-		static Project NewAcademyConversionProject()
-		{
-			// Initialing details, would use AutoFixture but these details are init only
-			// So we could do it on init but the School name for example would be unintelligible 
-			var projectDetails = new ProjectDetails()
+			var newAcademyConversionProject = NewAcademyConversionProject();
+			using var dbContextTransaction = await academisationContext.Database.BeginTransactionAsync();
+			try
 			{
-				Urn = (int)Faker.Number.Between(1, 100000),
-				SchoolName = Faker.Ancient.God() + " " + Faker.Company.Profession(),
-				ApplicationReferenceNumber = $"A2B_{Faker.Number.Between(1, 100000)}",
-				ProjectStatus = ProjectConsts.Statuses[(int)Faker.Number.Between(0, 7)],
-				ApplicationReceivedDate = DateTime.Now.AddMonths(-2),
-				HeadTeacherBoardDate = DateTime.Now.AddMonths(-2),
-				LocalAuthorityInformationTemplateSentDate = DateTime.Now.AddYears(-2),
-				LocalAuthorityInformationTemplateReturnedDate = DateTime.Now.AddYears(-1),
-				ProposedAcademyOpeningDate = DateTime.Now.AddMonths(3),
-				Region = ProjectConsts.Regions[(int)Faker.Number.Between(0, 9)],
-			};
+				// Create and add project
+				var newProjectState = ProjectState.MapFromDomain(newAcademyConversionProject);
+				newProjectState.Id = default;
+				newProjectStates.Add(newProjectState);
 
-			var newProject = new Project(Convert.ToInt32(Faker.Number.Number(7)), projectDetails);
-			return newProject;
+				if (newProjectStates.Count() % 80 == 0) // let's assume we want to batch insert every 20 items
+				{
+					academisationContext.Projects.AddRange(newProjectStates);
+					await academisationContext.SaveChangesAsync();
+
+					foreach (var projectState in newProjectStates)
+					{
+						var newProjectNote = fixture.Create<ProjectNoteState>();
+						newProjectNote.Id = default;
+						newProjectNote.ProjectId = projectState.Id;
+						projectNotes.Add(newProjectNote);
+					}
+
+					academisationContext.ProjectNotes.AddRange(projectNotes);
+					await academisationContext.SaveChangesAsync();
+					newProjectStates.Clear();
+					projectNotes.Clear();
+				}
+
+				await dbContextTransaction.CommitAsync();
+			}
+			catch (Exception ex)
+			{
+				await dbContextTransaction.RollbackAsync();
+				// better logging and error handling strategy
+				Console.WriteLine(ex.ToString());
+				throw;
+			}
 		}
 	}
+
+	static Project NewAcademyConversionProject()
+	{
+		// Initialing details, would use AutoFixture but these details are init only
+		// So we could do it on init but the School name for example would be unintelligible 
+		var projectDetails = new ProjectDetails()
+		{
+			Urn = (int)Faker.Number.Between(1, 100000),
+			SchoolName = Faker.Ancient.God() + " " + Faker.Company.Profession(),
+			ApplicationReferenceNumber = $"A2B_{Faker.Number.Between(1, 100000)}",
+			ProjectStatus = ProjectConsts.Statuses[(int)Faker.Number.Between(0, 7)],
+			ApplicationReceivedDate = DateTime.Now.AddMonths(-2),
+			HeadTeacherBoardDate = DateTime.Now.AddMonths(-2),
+			LocalAuthorityInformationTemplateSentDate = DateTime.Now.AddYears(-2),
+			LocalAuthorityInformationTemplateReturnedDate = DateTime.Now.AddYears(-1),
+			ProposedAcademyOpeningDate = DateTime.Now.AddMonths(3),
+			Region = ProjectConsts.Regions[(int)Faker.Number.Between(0, 9)],
+		};
+
+		var newProject = new Project(Convert.ToInt32(Faker.Number.Number(7)), projectDetails);
+		return newProject;
+	}
 }
+
+
