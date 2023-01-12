72c1aa3abab6c4239dcddd829d9cc29d37360063
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-01-12 11:11:07 +0000

    Formatting changes within Seed Program

--- Dfe.Academies.Academisation.Seed/Program.cs
+++ Dfe.Academies.Academisation.Seed/Program.cs
@@ -4,13 +4,9 @@ using Dfe.Academies.Academisation.Seed;
 
 // Set up Academisation context
 AcademisationContext? context = DatabaseConfig.InitialiseDbContext();
-if (context is null)
-{
-	throw new ArgumentNullException("Context");
-}
 
 // Set up Stopwatch
-	var stopwatch = new Stopwatch();
+var stopwatch = new Stopwatch();
 
 // Get user input on number of projects to create
 Console.WriteLine("Hello, please enter a number of projects you would like to seed.");
@@ -20,10 +16,7 @@ stopwatch.Start();
 
 // Begin creation of projects and project notes for the input number
 
-	await SeedProject.CreateProject(context, numberOfProjects);
-	stopwatch.Stop();
+await SeedProject.CreateProject(context ?? throw new InvalidOperationException(), numberOfProjects);
+stopwatch.Stop();
 
-	Console.WriteLine($"Done in {stopwatch.Elapsed.Minutes} minutes and {stopwatch.Elapsed.Seconds} seconds");
-
-
-
+Console.WriteLine($"Done in {stopwatch.Elapsed.Minutes} minutes and {stopwatch.Elapsed.Seconds} seconds");
