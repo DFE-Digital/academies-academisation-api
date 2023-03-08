38dea70fb5ad1494f8c11750878d6673e15b7d4e
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-03-08 13:38:49 +0000

    Removed rulefor as schools are added one by one

--- Dfe.Academies.Academisation.Domain/Validations/UpdateApplicationValidator.cs
+++ Dfe.Academies.Academisation.Domain/Validations/UpdateApplicationValidator.cs
@@ -27,11 +27,6 @@ public class UpdateApplicationValidator
 			.WithMessage("Cannot add more than one school when joining a multi academy trust.")
 			.OverridePropertyName(nameof(Application.ApplicationType));
 
-		RuleFor(x => x.existing.ApplicationType)
-			.Must(x => false)
-			.When(x => x.type == ApplicationType.FormAMat && x.schools.Count() <= 1)
-			.WithMessage("Must add more than one school when forming a multi academy trust.")
-			.OverridePropertyName(nameof(Application.ApplicationType));
 
 		RuleFor(x => x.status)
 			.Must(x => false)
