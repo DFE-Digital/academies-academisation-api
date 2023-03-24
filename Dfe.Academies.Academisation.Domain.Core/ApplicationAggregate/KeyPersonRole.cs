3355ee7ee9de9b468f579393bd32a9ec0677144c
dneed-nimble <Dominic.NEED@nimbleapproach.com>
2023-03-24 15:19:37 +0000

    Updated KeyPersonRole to have Member as the front end A2B provides it as an option

--- Dfe.Academies.Academisation.Domain.Core/ApplicationAggregate/KeyPersonRole.cs
+++ Dfe.Academies.Academisation.Domain.Core/ApplicationAggregate/KeyPersonRole.cs
@@ -17,7 +17,9 @@ namespace Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate
 		FinancialDirector = 3,
 		[Description("Trustee")]
 		Trustee = 4,
+		[Description("Member")]
+		Member = 5,
 		[Description("Other")]
-		Other = 5
+		Other = 6
 	}
 }
