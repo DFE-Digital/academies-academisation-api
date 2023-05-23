0fc67b5672052b956967b7733a0768ca5a35e3a4
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-05-23 17:00:19 +0100

    Add option to override CDN Host Header
    - This is useful if a proxy sits behind the CDN and an operator needs to route traffic
    via the proxy before it reaches the container app.

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -30,6 +30,7 @@ module "azure_container_apps_hosting" {
   cdn_frontdoor_host_add_response_headers         = local.cdn_frontdoor_host_add_response_headers
   cdn_frontdoor_custom_domains                    = local.cdn_frontdoor_custom_domains
   cdn_frontdoor_origin_fqdn_override              = local.cdn_frontdoor_origin_fqdn_override
+  cdn_frontdoor_origin_host_header_override       = local.cdn_frontdoor_origin_host_header_override
 
   enable_monitoring               = local.enable_monitoring
   monitor_email_receivers         = local.monitor_email_receivers
