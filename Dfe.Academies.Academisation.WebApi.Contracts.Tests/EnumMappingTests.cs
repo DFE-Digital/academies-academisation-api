73ec7a942f8737cdf9fdce65fce5597096cc847e
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-05-25 17:55:32 +0100

    Tests to make sure contract mapping profile is correct. Tests to make sure contract enums can map to domain enums

---
+++ Dfe.Academies.Academisation.WebApi.Contracts.Tests/EnumMappingTests.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Dfe.Academies.Academisation.WebApi.AutoMapper;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Dfe.Academies.Academisation.WebApi.Contracts.Tests
+{
+	public class EnumMappingTests
+	{
+		private readonly IMapper _mapper;
+
+		public EnumMappingTests()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				cfg.AddProfile<AutoMapperProfile>();
+			});
+			_mapper = config.CreateMapper();
+		}
+
+		[Theory]
+		[MemberData(nameof(YieldEnumValues), typeof(FromDomain.AdvisoryBoardDecision))]
+		public void AdvisoryBoardDecision_Should_Map_To_DomainCore(int enumValue)
+		{
+			var contract = (FromDomain.AdvisoryBoardDecision)enumValue;
+			Assert_ContractEnum_MapsTo_DomainCoreEnum<FromDomain.AdvisoryBoardDecision, Domain.Core.ConversionAdvisoryBoardDecisionAggregate.AdvisoryBoardDecision>(contract, enumValue);
+		}
+
+
+		[Theory]
+		[MemberData(nameof(YieldEnumValues), typeof(FromDomain.AdvisoryBoardDeclinedReason))]
+		public void AdvisoryBoardDeclinedReason_Should_Map_To_DomainCore(int enumValue)
+		{
+			var contract = (FromDomain.AdvisoryBoardDeclinedReason)enumValue;
+			Assert_ContractEnum_MapsTo_DomainCoreEnum<FromDomain.AdvisoryBoardDeclinedReason, Domain.Core.ConversionAdvisoryBoardDecisionAggregate.AdvisoryBoardDeclinedReason>(contract, enumValue);
+		}
+
+		[Theory]
+		[MemberData(nameof(YieldEnumValues), typeof(FromDomain.AdvisoryBoardDeferredReason))]
+		public void AdvisoryBoardDeferredReason_Should_Map_To_DomainCore(int enumValue)
+		{
+			var contract = (FromDomain.AdvisoryBoardDeferredReason)enumValue;
+			Assert_ContractEnum_MapsTo_DomainCoreEnum<FromDomain.AdvisoryBoardDeferredReason, Domain.Core.ConversionAdvisoryBoardDecisionAggregate.AdvisoryBoardDeferredReason>(contract, enumValue);
+		}
+
+		[Theory]
+		[MemberData(nameof(YieldEnumValues), typeof(FromDomain.ApplicationType))]
+		public void ApplicationType_Should_Map_To_DomainCore(int enumValue)
+		{
+			var contract = (FromDomain.ApplicationType)enumValue;
+			Assert_ContractEnum_MapsTo_DomainCoreEnum<FromDomain.ApplicationType, Domain.Core.ApplicationAggregate.ApplicationType>(contract, enumValue);
+		}
+
+		[Theory]
+		[MemberData(nameof(YieldEnumValues), typeof(FromDomain.ContributorRole))]
+		public void ContributorRole_Should_Map_To_DomainCore(int enumValue)
+		{
+			var contract = (FromDomain.ContributorRole)enumValue;
+			Assert_ContractEnum_MapsTo_DomainCoreEnum<FromDomain.ContributorRole, Domain.Core.ApplicationAggregate.ContributorRole>(contract, enumValue);
+		}
+
+		[Theory]
+		[MemberData(nameof(YieldEnumValues), typeof(FromDomain.DecisionMadeBy))]
+		public void DecisionMadeBy_Should_Map_To_DomainCore(int enumValue)
+		{
+			var contract = (FromDomain.DecisionMadeBy)enumValue;
+			Assert_ContractEnum_MapsTo_DomainCoreEnum<FromDomain.DecisionMadeBy, Domain.Core.ConversionAdvisoryBoardDecisionAggregate.DecisionMadeBy>(contract, enumValue);
+		}
+
+		public static IEnumerable<object[]> YieldEnumValues(Type enumType)
+		{
+			foreach (object? number in Enum.GetValues(enumType))
+			{
+				yield return new object[] { number };
+			}
+		}
+
+		private void Assert_ContractEnum_MapsTo_DomainCoreEnum<TContract, TDomain>(TContract contractEnumVal, int enumValue)
+			where TContract : Enum
+			where TDomain : Enum
+		{
+			using var scope = new AssertionScope();
+			scope.AddReportable("inputs", $"Contract Enum type: {typeof(TContract).Name}, Domain Enum type: {typeof(TDomain).Name}, int value: {enumValue}");
+
+			Enum.IsDefined(typeof(TContract), enumValue).Should().BeTrue();
+			Enum.IsDefined(typeof(TDomain), enumValue).Should().BeTrue();
+
+			var domainVal = _mapper.Map<TDomain>(contractEnumVal);
+
+			domainVal.ToString().Should().Be(contractEnumVal.ToString());
+		}
+	}
+}
--- Dfe.Academies.Academisation.WebApi.Contracts.Tests/MappingProfileTests.cs
+++ Dfe.Academies.Academisation.WebApi.Contracts.Tests/MappingProfileTests.cs
@@ -1,8 +1,7 @@
-﻿using AutoFixture;
-using AutoMapper;
-using AutoMapper.Execution;
+﻿using AutoMapper;
 using Dfe.Academies.Academisation.WebApi.AutoMapper;
 using FluentAssertions;
+using FluentAssertions.Execution;
 
 namespace Dfe.Academies.Academisation.WebApi.Contracts.Tests
 {
@@ -14,127 +13,7 @@ namespace Dfe.Academies.Academisation.WebApi.Contracts.Tests
 			CreateMapperConfiguration().AssertConfigurationIsValid();
 		}
 
-		[Theory]
-		[MemberData(nameof(AdvisoryBoardDecisionEnumValues))]
-		public void AdvisoryBoardDecision_Should_Map_To_DomainCore(int enumValue)
-		{
-			var mapper = CreateMapper();
-			
-			var contract = (Contracts.FromDomain.AdvisoryBoardDecision)enumValue;
-			var domain = mapper.Map<Domain.Core.ConversionAdvisoryBoardDecisionAggregate.AdvisoryBoardDecision>(contract);
-
-			((int)domain).Should().Be((int)contract);
-		}
-		
-		[Theory]
-		[MemberData(nameof(AdvisoryBoardDeclinedReasonEnumValues))]
-		public void AdvisoryBoardDeclinedReason_Should_Map_To_DomainCore(int enumValue)
-		{
-			var mapper = CreateMapper();
-			
-			var contract = (Contracts.FromDomain.AdvisoryBoardDecision)enumValue;
-			var domain = mapper.Map<Domain.Core.ConversionAdvisoryBoardDecisionAggregate.AdvisoryBoardDeclinedReason>(contract);
-
-			((int)domain).Should().Be((int)contract);
-		}	
-		
-		[Theory]
-		[MemberData(nameof(AdvisoryBoardDeclinedReasonEnumValues))]
-		public void AdvisoryBoardDeferredReason_Should_Map_To_DomainCore(int enumValue)
-		{
-			var mapper = CreateMapper();
-			
-			var contract = (Contracts.FromDomain.AdvisoryBoardDeferredReason)enumValue;
-			var domain = mapper.Map<Domain.Core.ConversionAdvisoryBoardDecisionAggregate.AdvisoryBoardDeferredReason>(contract);
-
-			((int)domain).Should().Be((int)contract);
-		}
-		
-		[Theory]
-		[MemberData(nameof(ApplicationTypeEnumValues))]
-		public void ApplicationType_Should_Map_To_DomainCore(int enumValue)
-		{
-			var mapper = CreateMapper();
-			
-			var contract = (Contracts.FromDomain.ApplicationType)enumValue;
-			var domain = mapper.Map<Domain.Core.ApplicationAggregate.ApplicationType>(contract);
-
-			((int)domain).Should().Be((int)contract);
-		}
-				
-		[Theory]
-		[MemberData(nameof(ContributorRoleEnumValues))]
-		public void ContributorRole_Should_Map_To_DomainCore(int enumValue)
-		{
-			var mapper = CreateMapper();
-			
-			var contract = (Contracts.FromDomain.ContributorRole)enumValue;
-			var domain = mapper.Map<Domain.Core.ApplicationAggregate.ContributorRole>(contract);
-
-			((int)domain).Should().Be((int)contract);
-		}	
-		
-		[Theory]
-		[MemberData(nameof(DecisionMadeByEnumValues))]
-		public void DecisionMadeBy_Should_Map_To_DomainCore(int enumValue)
-		{
-			var mapper = CreateMapper();
-			
-			var contract = (Contracts.FromDomain.ContributorRole)enumValue;
-			var domain = mapper.Map<Domain.Core.ApplicationAggregate.ContributorRole>(contract);
-
-			((int)domain).Should().Be((int)contract);
-		}
-
-		public static IEnumerable<object[]> AdvisoryBoardDecisionEnumValues()
-		{
-			foreach (var number in Enum.GetValues(typeof(Contracts.FromDomain.AdvisoryBoardDecision)))
-			{
-				yield return new object[] { number };
-			}
-		}		
-		
-		public static IEnumerable<object[]> AdvisoryBoardDeclinedReasonEnumValues()
-		{
-			foreach (var number in Enum.GetValues(typeof(Contracts.FromDomain.AdvisoryBoardDeclinedReason)))
-			{
-				yield return new object[] { number };
-			}
-		}		
-		
-		public static IEnumerable<object[]> DecisionMadeByEnumValues()
-		{
-			foreach (var number in Enum.GetValues(typeof(Contracts.FromDomain.DecisionMadeBy)))
-			{
-				yield return new object[] { number };
-			}
-		}
-		
-		public static IEnumerable<object[]> AdvisoryBoardDeferredReasonEnumValues()
-		{
-			foreach (var number in Enum.GetValues(typeof(Contracts.FromDomain.AdvisoryBoardDeferredReason)))
-			{
-				yield return new object[] { number };
-			}
-		}
-		
-		public static IEnumerable<object[]> ApplicationTypeEnumValues()
-		{
-			foreach (var number in Enum.GetValues(typeof(Contracts.FromDomain.ApplicationType)))
-			{
-				yield return new object[] { number };
-			}
-		}
-		
-		public static IEnumerable<object[]> ContributorRoleEnumValues()
-		{
-			foreach (var number in Enum.GetValues(typeof(Contracts.FromDomain.ContributorRole)))
-			{
-				yield return new object[] { number };
-			}
-		}
-
-        private IMapper CreateMapper()
+		private IMapper CreateMapper()
 		{
 			return CreateMapperConfiguration().CreateMapper();
 		}
@@ -146,13 +25,12 @@ namespace Dfe.Academies.Academisation.WebApi.Contracts.Tests
 				cfg.AddProfile<SubProfile>();
 			});
 		}
-
 		internal class SubProfile : Profile
 		{
 			public SubProfile()
 			{
 				ContractMappings.AddMappings(this);
-			} 
+			}
 		}
 	}
 }
