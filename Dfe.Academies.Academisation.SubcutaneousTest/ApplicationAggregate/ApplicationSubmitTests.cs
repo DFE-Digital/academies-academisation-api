01ef94ec84c66662508883f73c423928c3870397
danielryannimble <danielryan@nimbleapproach.com>
2023-06-22 16:41:26 +0100

    fix tests

--- Dfe.Academies.Academisation.SubcutaneousTest/ApplicationAggregate/ApplicationSubmitTests.cs
+++ Dfe.Academies.Academisation.SubcutaneousTest/ApplicationAggregate/ApplicationSubmitTests.cs
@@ -218,9 +218,7 @@ public class ApplicationSubmitTests
 	{
 		Assert.Multiple(
 		() => Assert.Equal("Converter Pre-AO (C)", project.ProjectStatus),
-		() => Assert.Equal(DateTime.Today.AddMonths(6), project.OpeningDate),
 		() => Assert.Equal(type, project.AcademyTypeAndRoute),
-		() => Assert.Equal(school.SchoolConversionTargetDate, project.ProposedAcademyOpeningDate),
 		() => Assert.Equal(25000.0m, project.ConversionSupportGrantAmount),
 		() => Assert.Equal(school.SchoolCapacityPublishedAdmissionsNumber.ToString(), project.PublishedAdmissionNumber),
 		() => Assert.Equal(ToYesNoString(school.LandAndBuildings!.PartOfPfiScheme), project.PartOfPfiScheme),
