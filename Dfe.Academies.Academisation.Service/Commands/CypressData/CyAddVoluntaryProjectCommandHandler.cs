638c5022aef8f48027098428c7afeff9c6f5b3df
Dominic Need <dominic.need@nimbleapproach.com>
2023-06-30 09:28:24 +0100

    Added a familiar project state for cypress setup/teardown

--- Dfe.Academies.Academisation.Service/Commands/CypressData/CyAddVoluntaryProjectCommandHandler.cs
+++ Dfe.Academies.Academisation.Service/Commands/CypressData/CyAddVoluntaryProjectCommandHandler.cs
@@ -1,7 +1,7 @@
 ﻿using Dfe.Academies.Academisation.Core;
+using Dfe.Academies.Academisation.Core.Utils;
 using Dfe.Academies.Academisation.Data;
 using Dfe.Academies.Academisation.Data.ProjectAggregate;
-using Dfe.Academies.Academisation.IDomain.ProjectAggregate;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,14 +13,13 @@ namespace Dfe.Academies.Academisation.Service.Commands.CypressData
 	public class CyAddVoluntaryProjectCommandHandler : CypressDataBaseCommandHandlerAbstractBase,
 		IRequestHandler<CyAddVoluntaryProjectCommand, CommandResult>
 	{
-		private readonly IProjectFactory _projectFactory;
+
 		/// <summary>
 		///     Initializes a new instance of the <see cref="CyAddVoluntaryProjectCommandHandler" /> class.
 		/// </summary>
 		/// <param name="dbContext">The db context.</param>
-		public CyAddVoluntaryProjectCommandHandler(AcademisationContext dbContext, IProjectFactory projectFactory) : base(dbContext)
+		public CyAddVoluntaryProjectCommandHandler(AcademisationContext dbContext) : base(dbContext)
 		{
-			_projectFactory = projectFactory;
 		}
 
 		/// <summary>
@@ -45,7 +44,37 @@ namespace Dfe.Academies.Academisation.Service.Commands.CypressData
 			}
 
 			// Create a new project
-			var newProject = new ProjectState { SchoolName = projectName };
+			var newProject = new ProjectState
+			{
+				SchoolName = projectName,
+				Urn = 139292,
+				ApplicationReferenceNumber = "A2B_1357",
+				ProjectStatus = "Approved with conditions",
+				ApplicationReceivedDate = new DateTime(2022, 3, 17),
+				HeadTeacherBoardDate = new DateTime(2023, 1, 1),
+				LocalAuthorityInformationTemplateSentDate = new DateTime(2019, 3, 21),
+				LocalAuthorityInformationTemplateReturnedDate = new DateTime(2020, 2, 20),
+				RecommendationForProject = "Approve",
+				AcademyOrderRequired = "Yes",
+				AcademyTypeAndRoute = "Converter",
+				ProposedAcademyOpeningDate = new DateTime(2025, 2, 20),
+				ConversionSupportGrantAmount = 25000,
+				PublishedAdmissionNumber = "60673",
+				ViabilityIssues = "No",
+				FinancialDeficit = "No",
+				DistanceFromSchoolToTrustHeadquarters = 10,
+				RevenueCarryForwardAtEndMarchCurrentYear = -10,
+				ProjectedRevenueBalanceAtEndMarchNextYear = -10,
+				CapitalCarryForwardAtEndMarchCurrentYear = -10,
+				CapitalCarryForwardAtEndMarchNextYear = -10,
+				IfdPipelineId = 0,
+				YearOneProjectedPupilNumbers = 104,
+				YearTwoProjectedPupilNumbers = 239,
+				YearThreeProjectedPupilNumbers = 370,
+				CreatedOn = DateTime.Now,
+				Region = "West Midlands",
+				LocalAuthority = "Coventry"
+			};
 
 			// Add the new project to the Projects DbSet
 			DbContext.Projects.Add(newProject);
