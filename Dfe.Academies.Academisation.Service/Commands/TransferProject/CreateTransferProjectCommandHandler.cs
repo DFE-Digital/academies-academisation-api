ba6f7f106a40799d04d819bb45116bd2e9fa7b05
plockwood <paul.lockwood@education.gov.uk>
2023-08-07 17:28:09 +0100

    refactoring of variable

--- Dfe.Academies.Academisation.Service/Commands/TransferProject/CreateTransferProjectCommandHandler.cs
+++ Dfe.Academies.Academisation.Service/Commands/TransferProject/CreateTransferProjectCommandHandler.cs
@@ -1,15 +1,9 @@
 ﻿using AutoMapper;
 using Dfe.Academies.Academisation.Core;
 using Dfe.Academies.Academisation.Core.Utils;
-using Dfe.Academies.Academisation.Domain.ApplicationAggregate;
-using Dfe.Academies.Academisation.Domain.Core.ApplicationAggregate;
 using Dfe.Academies.Academisation.Domain.TransferProjectAggregate;
-using Dfe.Academies.Academisation.IDomain.ApplicationAggregate;
-using Dfe.Academies.Academisation.IService.Commands.AdvisoryBoardDecision;
-using Dfe.Academies.Academisation.IService.RequestModels;
 using Dfe.Academies.Academisation.IService.ServiceModels.TransferProject;
 using Dfe.Academies.Academisation.Service.Commands.TransferProject;
-using Dfe.Academies.Academisation.Service.Mappers.Application;
 using Dfe.Academies.Academisation.Service.Mappers.TransferProject;
 using MediatR;
 
@@ -30,15 +24,15 @@ public class CreateTransferProjectCommandHandler : IRequestHandler<CreateTransfe
 
 	public async Task<CreateResult> Handle(CreateTransferProjectCommand request, CancellationToken cancellationToken)
 	{
-		var result = Domain.TransferProjectAggregate.TransferProject.Create(request.OutgoingTrustUkprn, request.IncomingTrustUkprn, request.TransferringAcademyUkprns, _dateTimeProvider.Now);
+		var transferProject = Domain.TransferProjectAggregate.TransferProject.Create(request.OutgoingTrustUkprn, request.IncomingTrustUkprn, request.TransferringAcademyUkprns, _dateTimeProvider.Now);
 
-		 _transferProjectRepository.Insert(result);
+		 _transferProjectRepository.Insert(transferProject);
 		await _transferProjectRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
 
-		result.GenerateUrn();
+		transferProject.GenerateUrn();
 
 		await _transferProjectRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
 
-		return new CreateSuccessResult<AcademyTransferProjectResponse>(AcademyTransferProjectResponseFactory.Create(result));
+		return new CreateSuccessResult<AcademyTransferProjectResponse>(AcademyTransferProjectResponseFactory.Create(transferProject));
 	}
 }
