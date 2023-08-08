c84c719718cfc631513c711260455d9d2721738b
Dominic Need <dominic.need@nimbleapproach.com>
2023-08-08 11:13:12 +0100

    Refactor for the renaming elements of School Overview

--- Dfe.Academies.Academisation.Service/Mappers/Legacy/ProjectAggregate/LegacyProjectServiceModelMapper.cs
+++ Dfe.Academies.Academisation.Service/Mappers/Legacy/ProjectAggregate/LegacyProjectServiceModelMapper.cs
@@ -52,7 +52,7 @@ internal static class LegacyProjectServiceModelMapper
 			ConversionSupportGrantChangeReason = project.Details.ConversionSupportGrantChangeReason,
 			Region = project.Details.Region,
 
-			// general info
+			// School Overview
 			SchoolPhase = project.Details.SchoolPhase,
 			AgeRange = project.Details.AgeRange,
 			SchoolType = project.Details.SchoolType,
@@ -68,9 +68,8 @@ internal static class LegacyProjectServiceModelMapper
 			PercentageOfGoodOrOutstandingSchoolsInTheDiocesanTrust = project.Details.PercentageOfGoodOrOutstandingSchoolsInTheDiocesanTrust,
 			DistanceFromSchoolToTrustHeadquarters = project.Details.DistanceFromSchoolToTrustHeadquarters,
 			DistanceFromSchoolToTrustHeadquartersAdditionalInformation = project.Details.DistanceFromSchoolToTrustHeadquartersAdditionalInformation,
-			MemberOfParliamentParty = project.Details.MemberOfParliamentParty,
-			MemberOfParliamentName = project.Details.MemberOfParliamentName,
-			GeneralInformationSectionComplete = project.Details.GeneralInformationSectionComplete,
+			MemberOfParliamentNameAndParty = project.Details.MemberOfParliamentNameAndParty,
+			SchoolOverviewSectionComplete = project.Details.SchoolOverviewSectionComplete,
 
 			// Annex B
 			AnnexBFormReceived = project.Details.AnnexBFormReceived,
