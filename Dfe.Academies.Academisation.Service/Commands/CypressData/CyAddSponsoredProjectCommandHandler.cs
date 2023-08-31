951d9f2ea71aa15021f22ec3735a783776e58650
Nick Warms <Nicholas.WARMS@education.gov.uk>
2023-08-31 10:34:09 +0100

    Rename project to Sponsored Cypress Project

--- Dfe.Academies.Academisation.Service/Commands/CypressData/CyAddSponsoredProjectCommandHandler.cs
+++ Dfe.Academies.Academisation.Service/Commands/CypressData/CyAddSponsoredProjectCommandHandler.cs
@@ -30,7 +30,7 @@ namespace Dfe.Academies.Academisation.Service.Commands.CypressData
 		public async Task<CommandResult> Handle(CyAddSponsoredProjectCommand request, CancellationToken cancellationToken)
 		{
 			// Define the project name
-			const string projectName = "Cypress Project";
+			const string projectName = "Sponsored Cypress Project";
 
 			// Find the project
 			var existingProject = await DbContext.Projects
