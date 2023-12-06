54715c745d6ebf6c027b2207881fc110485ea250
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2023-12-06 10:41:45 +0000

    Tested spreadsheet export controller and service

---
+++ Dfe.Academies.Academisation.WebApi.UnitTest/Controller/ConversionProjectControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Dfe.Academies.Academisation.Data.ProjectAggregate;
+using Dfe.Academies.Academisation.IService.Commands.Legacy.Project;
+using Dfe.Academies.Academisation.IService.Query;
+using Dfe.Academies.Academisation.WebApi.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Dfe.Academies.Academisation.WebApi.UnitTest.Controller
+{
+	public class ConversionProjectControllerTests
+	{
+		private readonly Mock<ICreateNewProjectCommand> _mockCreateNewProjectCommand;
+		private readonly Mock<IConversionProjectQueryService> _mockConversionProjectQueryService;
+		private readonly Mock<IConversionProjectExportService> _mockConversionProjectExportService;
+		private readonly Mock<IMediator> _mockMediator;
+		private readonly ProjectController _controller;
+
+		public ConversionProjectControllerTests()
+		{
+			_mockCreateNewProjectCommand = new Mock<ICreateNewProjectCommand>();
+			_mockConversionProjectQueryService = new Mock<IConversionProjectQueryService>();
+			_mockConversionProjectExportService = new Mock<IConversionProjectExportService>();
+			_mockMediator = new Mock<IMediator>();
+			_controller = new ProjectController(_mockCreateNewProjectCommand.Object,
+												_mockConversionProjectQueryService.Object,
+												_mockMediator.Object,
+												_mockConversionProjectExportService.Object);
+		}
+		[Fact]
+		public async Task ExportProjectsToSpreadsheet_ReturnsFileResult_WhenProjectsFound()
+		{
+			// Arrange
+			var mockStream = new MemoryStream();
+			var searchModel = new GetAcademyConversionSearchModel(1, 10, null, null, null, null, null);
+			_mockConversionProjectExportService.Setup(s => s.ExportProjectsToSpreadsheet(searchModel))
+											   .ReturnsAsync(mockStream);
+
+			// Act
+			var result = await _controller.ExportProjectsToSpreadsheet(searchModel);
+
+			// Assert
+			var fileResult = Assert.IsAssignableFrom<FileResult>(result);
+			Assert.Equal("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileResult.ContentType);
+			Assert.Equal("exported_projects.xlsx", fileResult.FileDownloadName);
+		}
+
+		[Fact]
+		public async Task ExportProjectsToSpreadsheet_ReturnsNotFound_WhenNoProjectsFound()
+		{
+			// Arrange
+			_mockConversionProjectExportService.Setup(s => s.ExportProjectsToSpreadsheet(It.IsAny<GetAcademyConversionSearchModel>()))
+											   .ReturnsAsync(null as Stream);
+			var searchModel = new GetAcademyConversionSearchModel(1, 10, null, null, null, null, null);
+
+			// Act
+			var result = await _controller.ExportProjectsToSpreadsheet(searchModel);
+
+			// Assert
+			var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+			Assert.Equal("No projects found for the specified criteria.", notFoundResult.Value);
+		}
+
+	}
+}
