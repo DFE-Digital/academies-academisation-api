aa27959565f36cfb448f8003daa6ca3d2092733e
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-03-01 11:15:13 +0000

    Add AcademyRoute to Transfer Export

--- Dfe.Academies.Academisation.Service/Queries/TransferProjectExportService.cs
+++ Dfe.Academies.Academisation.Service/Queries/TransferProjectExportService.cs
@@ -32,10 +32,10 @@ namespace Dfe.Academies.Academisation.Service.Queries
 
 		private static Task<Stream> GenerateSpreadsheet(IEnumerable<ExportedTransferProjectModel> projects)
 		{
-			string[] headers = new []{
+			string[] headers = new[]{
 				"School", "URN", "School Type", "Incoming Trust", "Outgoing Trust", "Incoming Trust UKPRN", "Local Authority", "Region",
 				"Advisory Board Date", "Decision Date", "Status", "Assigned To", "Reason for transfer", "Type of transfer",
-				"Proposed academy transfer date"
+				"Proposed academy transfer date", "Route"
 			};
 
 			var workbook = new XLWorkbook();
@@ -65,6 +65,7 @@ namespace Dfe.Academies.Academisation.Service.Queries
 				worksheet.Cell(row, 13).Value = project.TransferReason;
 				worksheet.Cell(row, 14).Value = project.TransferType;
 				worksheet.Cell(row, 15).Value = project.ProposedAcademyTransferDate;
+				worksheet.Cell(row, 16).Value = project.AcademyRoute;
 				row++;
 			}
 			worksheet.Columns().AdjustToContents();
