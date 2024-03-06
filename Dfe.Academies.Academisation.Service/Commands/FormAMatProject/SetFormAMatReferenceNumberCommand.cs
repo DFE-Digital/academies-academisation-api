b621e07f149d9596a5df785c544c5de0be73d73d
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2024-03-06 10:51:09 +0000

    Add service to apply FAM reference

---
+++ Dfe.Academies.Academisation.Service/Commands/FormAMatProject/SetFormAMatReferenceNumberCommand.cs
@@ -0,0 +1,9 @@
+﻿using Dfe.Academies.Academisation.Core;
+using MediatR;
+
+namespace Dfe.Academies.Academisation.Service.Commands.FormAMat
+{
+	public class SetFormAMatReferenceNumberCommand : IRequest<CommandResult>
+	{
+	}
+}
