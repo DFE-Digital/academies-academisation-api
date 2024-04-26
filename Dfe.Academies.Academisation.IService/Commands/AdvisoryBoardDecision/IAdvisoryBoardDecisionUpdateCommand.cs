d469f557e3181d282da85e18d32ca6b1b8b0bca5
plockwood <paul.lockwood@education.gov.uk>
2024-04-26 17:25:40 +0100

    folder removed, no longer needed

--- Dfe.Academies.Academisation.IService/Commands/AdvisoryBoardDecision/IAdvisoryBoardDecisionUpdateCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dfe.Academies.Academisation.Core;
-using Dfe.Academies.Academisation.IService.ServiceModels.ConversionAdvisoryBoardDecision;
-
-namespace Dfe.Academies.Academisation.IService.Commands.AdvisoryBoardDecision;
-
-public interface IAdvisoryBoardDecisionUpdateCommand
-{
-	Task<CommandResult> Execute(ConversionAdvisoryBoardDecisionServiceModel requestModel);
-}
