cf3392261829989b3e9a3de0562c65d2c3e94c1c
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-06-04 14:21:11 +0100

    Corrected syntax for versions.tf (#494)

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -9,9 +9,9 @@ terraform {
       source  = "StatusCakeDev/statuscake"
       version = ">= 2.1.0"
     }
-  }
-  azapi = {
-    source  = "Azure/azapi"
-    version = ">= 1.13.0"
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 1.13.0"
+    }
   }
 }
