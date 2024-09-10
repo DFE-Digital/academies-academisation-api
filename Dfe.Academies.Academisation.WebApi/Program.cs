82e65732a4eb80aaf9e996dc24b9f927a22d8d20
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-09-10 14:28:56 +0100

    Set HTTP user agent header

--- Dfe.Academies.Academisation.WebApi/Program.cs
+++ Dfe.Academies.Academisation.WebApi/Program.cs
@@ -213,6 +213,7 @@ builder.Services.AddHttpClient("AcademiesApi", (sp, client) =>
 	{
 		client.BaseAddress = new Uri(url);
 		client.DefaultRequestHeaders.Add("ApiKey", configuration.GetValue<string>("AcademiesApiKey"));
+		client.DefaultRequestHeaders.Add("User-Agent", "AcademisationApi/1.0");
 	}
 	else
 	{
