b71f51f2bed83badbfa732df51e2356304b3b608
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2025-03-28 14:59:58 +0000

    Fixed code smell

--- Dfe.Academies.Academisation.Service.UnitTest/Commands/TransferProject/SetTransferPublicSectorEqualityDutyCommandHandlerTests.cs
+++ Dfe.Academies.Academisation.Service.UnitTest/Commands/TransferProject/SetTransferPublicSectorEqualityDutyCommandHandlerTests.cs
@@ -17,87 +17,12 @@ namespace Dfe.Academies.Academisation.Service.UnitTest.Commands.TransferProject
 	public class SetTransferPublicSectorEqualityDutyCommandHandlerTests
 	{
 
-		/*
-		 
-		
-		
-	[Fact]
-	public async Task Handle_TransferProjectNotFound_ReturnsNotFoundCommandResult()
-	{
-		// Arrange
-		var command = new SetTransferProjectGeneralInformationCommand
-		{
-			Urn = 1,
-			Recommendation = "Recommendation",
-			Author = "Author"
-		};
-
-		_transferProjectRepositoryMock.Setup(x => x.GetById(It.IsAny<int>()))!
-			.ReturnsAsync((Domain.TransferProjectAggregate.TransferProject)null);
-
-		// Act
-		var result = await _handler.Handle(command, default);
-
-		// Assert
-		result.Should().BeOfType<NotFoundCommandResult>();
-		_loggerMock.Verify(logger =>
-				logger.Log(
-					LogLevel.Error,
-					It.IsAny<EventId>(),
-					It.Is<It.IsAnyType>((v, t) => true),
-					It.IsAny<Exception>(),
-					It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)!),
-			Times.Once);
-	}
-
-	[Fact]
-	public async Task Handle_TransferProjectFound_ReturnsCommandSuccessResult()
-	{
-		// Arrange
-		var command = new SetTransferProjectGeneralInformationCommand
-		{
-			Urn = 1,
-			Recommendation = "Recommendation",
-			Author = "Author"
-		};
-		// Create a transfer project to 'SetTrustInformationAndProjectDates' to
-		var transferringAcademies = new List<TransferringAcademy>() { new TransferringAcademy("23456789", "in trust", "34567890", "", "") };
-		var transferProject = Domain.TransferProjectAggregate.TransferProject.Create("12345678", "out trust", transferringAcademies, false, DateTime.Now);
-		// Mock Unit of work and Repository 
-		var unitOfWorkMock = new Mock<IUnitOfWork>();
-		unitOfWorkMock.Setup(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()))
-			.Returns(Task.FromResult(1));  // Return Task<int> with a dummy value
-		_transferProjectRepositoryMock.Setup(repo => repo.UnitOfWork)
-			.Returns(unitOfWorkMock.Object);
-
-		// Mock GetById to use our Transfer Project from above
-		_transferProjectRepositoryMock.Setup(x => x.GetByUrn(It.IsAny<int>())).ReturnsAsync(transferProject);
-
-
-		// Act
-		var result = await _handler.Handle(command, new CancellationToken(false));
-
-		// Assert
-		result.Should().BeOfType<CommandSuccessResult>();
-
-		transferProject.Recommendation.Should().Be(command.Recommendation);
-		transferProject.Author.Should().Be(command.Author);
-
-		_transferProjectRepositoryMock.Verify(repo => repo.Update(It.IsAny<Domain.TransferProjectAggregate.TransferProject>()), Times.Once);
-	}
-		  
-		 
-		 */
-
 		[Fact]
 		public async Task Handle_TransferProjectNotFound_ReturnsNotFoundCommandResult()
 		{
 			// Arrange
 			var transferProjectRepositoryMock = new Mock<ITransferProjectRepository>();
 			var loggerMock = new Mock<ILogger<SetTransferPublicSectorEqualityDutyCommandHandler>>();
-
-			transferProjectRepositoryMock.Setup(x => x.GetByUrn(It.IsAny<int>()))
-				.ReturnsAsync((Domain.TransferProjectAggregate.TransferProject)null);
 
 			var handler = new SetTransferPublicSectorEqualityDutyCommandHandler(transferProjectRepositoryMock.Object, loggerMock.Object);
 			var command = new SetTransferPublicSectorEqualityDutyCommand
