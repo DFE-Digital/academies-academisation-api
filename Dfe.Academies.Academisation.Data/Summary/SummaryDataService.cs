b938cf21475434d4526631606c8b8898783a648f
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2025-06-25 14:14:34 +0100

    Fix complexity smell

--- Dfe.Academies.Academisation.Data/Summary/SummaryDataService.cs
+++ Dfe.Academies.Academisation.Data/Summary/SummaryDataService.cs
@@ -14,76 +14,12 @@ namespace Dfe.Academies.Academisation.Data.Summary
 
             if (includeConversions)
             {
-                conversionQuery = _context.Projects
-                    .Where(x =>
-                        (x.Details.ProjectStatus == "Converter Pre-AO (C)" || x.Details.ProjectStatus == "Deferred") &&
-                        x.Details.AssignedUser != null &&
-                        x.Details.AssignedUser.EmailAddress == email
-                    )
-                    .Select(x => new ProjectSummaryIntermediate
-                    {
-                        Id = x.Id,
-                        Urn = x.Details.Urn,
-                        CreatedOn = x.CreatedOn,
-                        LastModifiedOn = x.LastModifiedOn,
-                        ConversionsSummary = new ConversionsSummary
-                        {
-                            ApplicationReferenceNumber = x.Details.ApplicationReferenceNumber,
-                            SchoolName = x.Details.SchoolName,
-                            LocalAuthority = x.Details.LocalAuthority,
-                            Region = x.Details.Region,
-                            AcademyTypeAndRoute = x.Details.AcademyTypeAndRoute,
-                            NameOfTrust = x.Details.NameOfTrust,
-                            AssignedUserEmailAddress = x.Details.AssignedUser != null ? x.Details.AssignedUser.EmailAddress : null,
-                            AssignedUserFullName = x.Details.AssignedUser != null ? x.Details.AssignedUser.FullName : null,
-                            ProjectStatus = x.Details.ProjectStatus,
-                            TrustReferenceNumber = x.Details.TrustReferenceNumber,
-                        },
-                        TransfersSummary = null
-                    });
+                conversionQuery = GetConversionQuery(email);
             }
 
             if (includeTransfers)
             {
-                transferQuery = _context.TransferProjects
-                    .Where(x => x.AssignedUserEmailAddress == email)
-                    .Select(x => new
-                    {
-                        x.Id,
-                        x.Urn,
-                        x.CreatedOn,
-                        x.LastModifiedOn,
-                        x.ProjectReference,
-                        x.OutgoingTrustUkprn,
-                        x.OutgoingTrustName,
-                        x.TypeOfTransfer,
-                        x.TargetDateForTransfer,
-                        x.AssignedUserEmailAddress,
-                        x.AssignedUserFullName,
-                        x.Status,
-                        FirstAcademy = x.TransferringAcademies.FirstOrDefault()
-                    })
-                    .Select(x => new ProjectSummaryIntermediate
-                    {
-                        Id = x.Id,
-                        Urn = x.Urn,
-                        CreatedOn = x.CreatedOn,
-                        LastModifiedOn = x.LastModifiedOn,
-                        ConversionsSummary = null,
-                        TransfersSummary = new TransfersSummary
-                        {
-                            ProjectReference = x.ProjectReference,
-                            OutgoingTrustUkprn = x.OutgoingTrustUkprn,
-                            OutgoingTrustName = x.OutgoingTrustName,
-                            TypeOfTransfer = x.TypeOfTransfer,
-                            TargetDateForTransfer = x.TargetDateForTransfer,
-                            AssignedUserEmailAddress = x.AssignedUserEmailAddress,
-                            AssignedUserFullName = x.AssignedUserFullName,
-                            Status = x.Status,
-                            IncomingTrustUkprn = x.FirstAcademy != null ? x.FirstAcademy.IncomingTrustUkprn : null,
-                            IncomingTrustName = x.FirstAcademy != null ? x.FirstAcademy.IncomingTrustName : null
-                        }
-                    });
+                transferQuery = GetTransferQuery(email);
             }
 
             var projectSummaries = conversionQuery
@@ -101,7 +37,80 @@ namespace Dfe.Academies.Academisation.Data.Summary
             return Task.FromResult(projectSummaries);
         }
 
-   	}
+        private IQueryable<ProjectSummaryIntermediate> GetTransferQuery(string email)
+        {
+	        return _context.TransferProjects
+		        .Where(x => x.AssignedUserEmailAddress == email)
+		        .Select(x => new
+		        {
+			        x.Id,
+			        x.Urn,
+			        x.CreatedOn,
+			        x.LastModifiedOn,
+			        x.ProjectReference,
+			        x.OutgoingTrustUkprn,
+			        x.OutgoingTrustName,
+			        x.TypeOfTransfer,
+			        x.TargetDateForTransfer,
+			        x.AssignedUserEmailAddress,
+			        x.AssignedUserFullName,
+			        x.Status,
+			        FirstAcademy = x.TransferringAcademies.FirstOrDefault()
+		        })
+		        .Select(x => new ProjectSummaryIntermediate
+		        {
+			        Id = x.Id,
+			        Urn = x.Urn,
+			        CreatedOn = x.CreatedOn,
+			        LastModifiedOn = x.LastModifiedOn,
+			        ConversionsSummary = null,
+			        TransfersSummary = new TransfersSummary
+			        {
+				        ProjectReference = x.ProjectReference,
+				        OutgoingTrustUkprn = x.OutgoingTrustUkprn,
+				        OutgoingTrustName = x.OutgoingTrustName,
+				        TypeOfTransfer = x.TypeOfTransfer,
+				        TargetDateForTransfer = x.TargetDateForTransfer,
+				        AssignedUserEmailAddress = x.AssignedUserEmailAddress,
+				        AssignedUserFullName = x.AssignedUserFullName,
+				        Status = x.Status,
+				        IncomingTrustUkprn = x.FirstAcademy != null ? x.FirstAcademy.IncomingTrustUkprn : null,
+				        IncomingTrustName = x.FirstAcademy != null ? x.FirstAcademy.IncomingTrustName : null
+			        }
+		        });
+        }
+
+        private IQueryable<ProjectSummaryIntermediate> GetConversionQuery(string email)
+        {
+	        return _context.Projects
+		        .Where(x =>
+			        (x.Details.ProjectStatus == "Converter Pre-AO (C)" || x.Details.ProjectStatus == "Deferred") &&
+			        x.Details.AssignedUser != null &&
+			        x.Details.AssignedUser.EmailAddress == email
+		        )
+		        .Select(x => new ProjectSummaryIntermediate
+		        {
+			        Id = x.Id,
+			        Urn = x.Details.Urn,
+			        CreatedOn = x.CreatedOn,
+			        LastModifiedOn = x.LastModifiedOn,
+			        ConversionsSummary = new ConversionsSummary
+			        {
+				        ApplicationReferenceNumber = x.Details.ApplicationReferenceNumber,
+				        SchoolName = x.Details.SchoolName,
+				        LocalAuthority = x.Details.LocalAuthority,
+				        Region = x.Details.Region,
+				        AcademyTypeAndRoute = x.Details.AcademyTypeAndRoute,
+				        NameOfTrust = x.Details.NameOfTrust,
+				        AssignedUserEmailAddress = x.Details.AssignedUser != null ? x.Details.AssignedUser.EmailAddress : null,
+				        AssignedUserFullName = x.Details.AssignedUser != null ? x.Details.AssignedUser.FullName : null,
+				        ProjectStatus = x.Details.ProjectStatus,
+				        TrustReferenceNumber = x.Details.TrustReferenceNumber,
+			        },
+			        TransfersSummary = null
+		        });
+        }
+	}
 	
 	public class ProjectSummaryIntermediate
 	{
