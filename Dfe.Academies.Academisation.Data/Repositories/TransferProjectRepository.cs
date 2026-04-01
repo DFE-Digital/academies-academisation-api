36519e470d68615a7e350d172e69833efbe6e6bd
paddy-clark <109068920+paddy-clark@users.noreply.github.com>
2026-04-01 13:12:21 +0100

    Revert "resolve pre-decision status filter not returning projects (#711)"
    This reverts commit f5b775056fd37dce98fd99b4518d256cdb741ea4.

--- Dfe.Academies.Academisation.Data/Repositories/TransferProjectRepository.cs
+++ Dfe.Academies.Academisation.Data/Repositories/TransferProjectRepository.cs
@@ -101,10 +101,7 @@ namespace Dfe.Academies.Academisation.Data.Repositories
 		{
 			if (states != null && states!.Any())
 			{
-				queryable = queryable.Where(p => 
-					states.Contains(p.Status!.ToLower())
-					|| (states.Contains("Pre advisory board") && p.Status! == null)
-				);
+				queryable = queryable.Where(p => states.Contains(p.Status!.ToLower()));
 			}
 
 			return queryable;
