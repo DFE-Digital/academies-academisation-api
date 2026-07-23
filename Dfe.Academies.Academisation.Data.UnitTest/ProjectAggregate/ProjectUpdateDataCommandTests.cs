056513ed85981c2438d1d8a5bae2036d05e22151
Ali <Muhammad.ALI-SHAH@education.gov.uk>
2026-07-23 16:16:21 +0100

    Test fixes

--- Dfe.Academies.Academisation.Data.UnitTest/ProjectAggregate/ProjectUpdateDataCommandTests.cs
+++ Dfe.Academies.Academisation.Data.UnitTest/ProjectAggregate/ProjectUpdateDataCommandTests.cs
@@ -45,6 +45,8 @@ namespace Dfe.Academies.Academisation.Data.UnitTest.ProjectAggregate
 				.With(x => x.ExternalApplicationFormUrl, "test//url")
 				.With(x => x.AcademyTypeAndRoute, "Sponsored")
 				.With(x => x.ConversionSupportGrantAmountChanged, false)
+				.With(x => x.HeadTeacherBoardDate, (DateTime?)DateTime.Today.AddDays(40))
+				.With(x => x.SfsoCommissioningRequestedDate, (DateTime?)DateTime.Today.AddDays(25)) // = HTB − 15, matches Update's derivation
 				.Create();
 
 
